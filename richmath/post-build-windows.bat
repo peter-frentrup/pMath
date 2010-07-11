fe1ec8d52a4b2934763c8c60a1f7148524dd2ede
Peter Frentrup <peter_frentrup@gmx.de>
2010-07-11 12:16:52 +0000

    added richmath

---
+++ richmath/post-build-windows.bat
@@ -0,0 +1,3 @@
+COPY /Y ..\library\bin\windows\%1\pmath*.dll bin\windows\%1
+ECHO Get(ToFileName({DirectoryName($Input, 5), "library", "scripts"}, "maininit.pmath")) > bin\windows\%1\maininit.pmath
+ECHO Get(ToFileName({DirectoryName($Input, 4), "scripts"}, "frontinit.pmath")) >> bin\windows\%1\maininit.pmath
