114e17b1bc3ce0b20916ff348f00da03c6967122
Peter Frentrup <peter_frentrup@gmx.de>
2011-03-03 20:08:59 +0000

    pmath:   - post build script now creates test bin directory if it does not already exist

--- library/post-build-windows.bat
+++ library/post-build-windows.bat
@@ -1,3 +1,4 @@
+MKDIR \test\bin\windows\%1
 MOVE /Y bin\windows\%1\libpmath.dll.def  bin\windows\%1\pmath.def
 COPY /Y bin\windows\%1\pmath*.dll        test\bin\windows\%1
 COPY /Y scripts\maininit.5.txt           test\bin\windows\%1\maininit.pmath
