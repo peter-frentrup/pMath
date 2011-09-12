a755473ef0976c1f08fa23dc3597587cfc9efa19
Peter Frentrup <peter_frentrup@gmx.de>
2011-09-12 21:40:43 +0000

    svn update fails at version-private.h ?!?

---
+++ library/include/pmath-util/version-private.h
@@ -0,0 +1,34 @@
+#ifndef __PMATH_UTIL__VERSION_PRIVATE_H__
+#define __PMATH_UTIL__VERSION_PRIVATE_H__
+
+	//Date Version Types
+	static const char _PMATH_VERSION_DATE[] = "12";
+	static const char _PMATH_VERSION_MONTH[] = "09";
+	static const char _PMATH_VERSION_YEAR[] = "2011";
+	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] = "11.09";
+	
+	//Software Status
+	static const char _PMATH_VERSION_STATUS[] = "Alpha";
+	static const char _PMATH_VERSION_STATUS_SHORT[] = "a";
+	
+	//Standard Version Type
+	static const long _PMATH_VERSION_MAJOR = 0;
+	static const long _PMATH_VERSION_MINOR = 2;
+	static const long _PMATH_VERSION_BUILD = 442;
+	static const long _PMATH_VERSION_REVISION = 442;
+	
+	//Miscellaneous Version Types
+	static const long _PMATH_VERSION_BUILDS_COUNT = 448;
+	#define _PMATH_VERSION_RC_FILEVERSION 0,2,442,442
+	#define _PMATH_VERSION_RC_FILEVERSION_STRING "0, 2, 442, 442\0"
+	static const char _PMATH_VERSION_FULLVERSION_STRING[] = "0.2.442.442";
+	
+	//SVN Version
+	static const char _PMATH_VERSION_SVN_REVISION[] = "266";
+	static const char _PMATH_VERSION_SVN_DATE[] = "2011-09-11T18:31:31.864619Z";
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	static const long _PMATH_VERSION_BUILD_HISTORY = 0;
+	
+
+#endif //__PMATH_UTIL__VERSION_PRIVATE_H__
