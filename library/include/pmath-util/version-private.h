700aaaccb17799fbcbe9383adf435bd8a464362b
Peter Frentrup <peter_frentrup@gmx.de>
2012-09-05 22:47:05 +0000

    pmath:   - updated documentation

--- library/include/pmath-util/version-private.h
+++ library/include/pmath-util/version-private.h
@@ -2,7 +2,7 @@
 #define __PMATH_UTIL__VERSION_PRIVATE_H__
 
 	//Date Version Types
-	static const char _PMATH_VERSION_DATE[] = "05";
+	static const char _PMATH_VERSION_DATE[] = "06";
 	static const char _PMATH_VERSION_MONTH[] = "09";
 	static const char _PMATH_VERSION_YEAR[] = "2012";
 	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] = "12.09";
@@ -18,14 +18,14 @@
 	static const long _PMATH_VERSION_REVISION = 447;
 	
 	//Miscellaneous Version Types
-	static const long _PMATH_VERSION_BUILDS_COUNT = 39;
+	static const long _PMATH_VERSION_BUILDS_COUNT = 40;
 	#define _PMATH_VERSION_RC_FILEVERSION 0,2,39,447
 	#define _PMATH_VERSION_RC_FILEVERSION_STRING "0, 2, 39, 447\0"
 	static const char _PMATH_VERSION_FULLVERSION_STRING[] = "0.2.39.447";
 	
 	//SVN Version
-	static const char _PMATH_VERSION_SVN_REVISION[] = "447";
-	static const char _PMATH_VERSION_SVN_DATE[] = "2012-08-18T14:19:24.191117Z";
+	static const char _PMATH_VERSION_SVN_REVISION[] = "444";
+	static const char _PMATH_VERSION_SVN_DATE[] = "2012-08-18T12:19:47.410117Z";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long _PMATH_VERSION_BUILD_HISTORY = 0;
