762edd1a28b3ed43440f32427baf63aa3a9a4083
Peter Frentrup <peter_frentrup@gmx.de>
2012-09-05 01:01:15 +0000

    pmath:   - The AutoVersioning plugin messed with mayor/minor numbers. Those should be set manually only! The Build number is unusable (it should be in the thousands, not 39) But at least Svn Revision number works

--- library/include/pmath-util/version-private.h
+++ library/include/pmath-util/version-private.h
@@ -2,26 +2,26 @@
 #define __PMATH_UTIL__VERSION_PRIVATE_H__
 
 	//Date Version Types
-	static const char _PMATH_VERSION_DATE[] = "30";
-	static const char _PMATH_VERSION_MONTH[] = "08";
+	static const char _PMATH_VERSION_DATE[] = "05";
+	static const char _PMATH_VERSION_MONTH[] = "09";
 	static const char _PMATH_VERSION_YEAR[] = "2012";
-	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] = "12.08";
+	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] = "12.09";
 	
 	//Software Status
 	static const char _PMATH_VERSION_STATUS[] = "Alpha";
 	static const char _PMATH_VERSION_STATUS_SHORT[] = "a";
 	
 	//Standard Version Type
-	static const long _PMATH_VERSION_MAJOR = 1;
-	static const long _PMATH_VERSION_MINOR = 0;
-	static const long _PMATH_VERSION_BUILD = 31;
-	static const long _PMATH_VERSION_REVISION = 31;
+	static const long _PMATH_VERSION_MAJOR = 0;
+	static const long _PMATH_VERSION_MINOR = 2;
+	static const long _PMATH_VERSION_BUILD = 39;
+	static const long _PMATH_VERSION_REVISION = 447;
 	
 	//Miscellaneous Version Types
-	static const long _PMATH_VERSION_BUILDS_COUNT = 37;
-	#define _PMATH_VERSION_RC_FILEVERSION 1,0,31,31
-	#define _PMATH_VERSION_RC_FILEVERSION_STRING "1, 0, 31, 31\0"
-	static const char _PMATH_VERSION_FULLVERSION_STRING[] = "1.0.31.31";
+	static const long _PMATH_VERSION_BUILDS_COUNT = 39;
+	#define _PMATH_VERSION_RC_FILEVERSION 0,2,39,447
+	#define _PMATH_VERSION_RC_FILEVERSION_STRING "0, 2, 39, 447\0"
+	static const char _PMATH_VERSION_FULLVERSION_STRING[] = "0.2.39.447";
 	
 	//SVN Version
 	static const char _PMATH_VERSION_SVN_REVISION[] = "447";
