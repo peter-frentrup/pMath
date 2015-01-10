fab315e9b234c219101122df19eb2d77427104fa
Peter Frentrup <frentrup@math.hu-berlin.de>
2015-01-10 14:54:51 +0100

    richmath: restructure build system to prepare Win64-port

--- richmath/post-build-linux.sh
+++ richmath/post-build-linux.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -x
+#
+# argument $1 is pmath library build name ( ../library/bin/linux/$1/* )
+# argument $2 is dependency architecture  ( dependencies/linux/$2/* )
+# argument $3 is target build name        ( bin/linux/$3/* )
+
+
 
 function ln-copy {
 	ln -sf "$1" "$2"
@@ -7,15 +13,15 @@ function ln-copy {
 	fi
 }
 
-cp ../library/bin/linux/$1/*.so.0.1  bin/linux/$2/
-cp depencies/linux/*                 bin/linux/$2/
+cp ../library/bin/linux/$1/*.so.0.1  bin/linux/$3/
+cp dependencies/linux/$2/*           bin/linux/$3/
 
 function ln-copy-all {
 	ln-copy $(pwd)/libpmath.so.0.1 libpmath.so.0
 	ln-copy $(pwd)/libpmath.so.0   libpmath.so
 }
 
-pushd bin/linux/$2
+pushd bin/linux/$3
 ln-copy-all
 
 # add $ORIGIN to the rpath. Due to the $ sign, this cannot reliably be done through Code::Blocks IDE
--- richmath/post-build-windows.bat
+++ richmath/post-build-windows.bat
@@ -1,2 +1,8 @@
-COPY /Y ..\library\bin\windows\%1\pmath*.dll bin\windows\%1\*
-COPY /Y depencies\windows\*                  bin\windows\%1\*
+
+REM  argument %1 is pmath library build name ( ..\library\bin\windows\%1\* )
+REM  argument %2 is dependency architecture  ( dependencies\windows\%2\* )
+REM  argument %3 is target build name        ( bin\windows\%3\* )
+
+
+COPY /Y ..\library\bin\windows\%1\pmath*.dll bin\windows\%3\*
+COPY /Y dependencies\windows\%2\*            bin\windows\%3\*
