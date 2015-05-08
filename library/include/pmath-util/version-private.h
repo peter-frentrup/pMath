f001b5d346dd8166c885498f2b74476d5ac1da1b
Peter Frentrup <frentrup@math.hu-berlin.de>
2015-05-08 23:08:39 +0200

    MSVC x64: use gmp/mpfr/mpfi dlls, so need __GMP_LIBGMP_DLL (TODO: do this on all Windows configurations)

--- library/include/pmath-util/version-private.h
+++ library/include/pmath-util/version-private.h
@@ -2,10 +2,10 @@
 #define __PMATH_UTIL__VERSION_PRIVATE_H__
 
 	//Date Version Types
-	static const char _PMATH_VERSION_DATE[] = "01";
-	static const char _PMATH_VERSION_MONTH[] = "12";
-	static const char _PMATH_VERSION_YEAR[] = "2014";
-	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] =  "14.12";
+	static const char _PMATH_VERSION_DATE[] = "08";
+	static const char _PMATH_VERSION_MONTH[] = "05";
+	static const char _PMATH_VERSION_YEAR[] = "2015";
+	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] =  "15.05";
 	
 	//Software Status
 	static const char _PMATH_VERSION_STATUS[] =  "Alpha";
@@ -14,14 +14,14 @@
 	//Standard Version Type
 	static const long _PMATH_VERSION_MAJOR  = 0;
 	static const long _PMATH_VERSION_MINOR  = 5;
-	static const long _PMATH_VERSION_BUILD  = 413;
-	static const long _PMATH_VERSION_REVISION  = 821;
+	static const long _PMATH_VERSION_BUILD  = 415;
+	static const long _PMATH_VERSION_REVISION  = 823;
 	
 	//Miscellaneous Version Types
-	static const long _PMATH_VERSION_BUILDS_COUNT  = 497;
-	#define _PMATH_VERSION_RC_FILEVERSION 0,5,413,821
-	#define _PMATH_VERSION_RC_FILEVERSION_STRING "0, 5, 413, 821\0"
-	static const char _PMATH_VERSION_FULLVERSION_STRING [] = "0.5.413.821";
+	static const long _PMATH_VERSION_BUILDS_COUNT  = 500;
+	#define _PMATH_VERSION_RC_FILEVERSION 0,5,415,823
+	#define _PMATH_VERSION_RC_FILEVERSION_STRING "0, 5, 415, 823\0"
+	static const char _PMATH_VERSION_FULLVERSION_STRING [] = "0.5.415.823";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long _PMATH_VERSION_BUILD_HISTORY  = 0;
