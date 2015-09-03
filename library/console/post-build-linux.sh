e1544f1aa729b02def194321d640e47c9953a4df
Peter Frentrup <frentrup@math.hu-berlin.de>
2015-09-03 16:58:32 +0200

    pmathc msvc x64 build

---
+++ library/console/post-build-linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+
+pushd bin/linux/$1
+
+# add $ORIGIN to the rpath. Due to the $ sign, this cannot be done through any more  Code::Blocks IDE with "-Wl,-rpath,\\$$ORIGIN"
+chrpath -r '$ORIGIN' ./pmathc
+
+popd
+
--- library/include/pmath-util/version-private.h
+++ library/include/pmath-util/version-private.h
@@ -2,10 +2,10 @@
 #define __PMATH_UTIL__VERSION_PRIVATE_H__
 
 	//Date Version Types
-	static const char _PMATH_VERSION_DATE[] = "14";
-	static const char _PMATH_VERSION_MONTH[] = "06";
+	static const char _PMATH_VERSION_DATE[] = "03";
+	static const char _PMATH_VERSION_MONTH[] = "09";
 	static const char _PMATH_VERSION_YEAR[] = "2015";
-	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] =  "15.06";
+	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] =  "15.09";
 	
 	//Software Status
 	static const char _PMATH_VERSION_STATUS[] =  "Alpha";
@@ -14,14 +14,14 @@
 	//Standard Version Type
 	static const long _PMATH_VERSION_MAJOR  = 0;
 	static const long _PMATH_VERSION_MINOR  = 5;
-	static const long _PMATH_VERSION_BUILD  = 487;
-	static const long _PMATH_VERSION_REVISION  = 895;
+	static const long _PMATH_VERSION_BUILD  = 489;
+	static const long _PMATH_VERSION_REVISION  = 897;
 	
 	//Miscellaneous Version Types
-	static const long _PMATH_VERSION_BUILDS_COUNT  = 590;
-	#define _PMATH_VERSION_RC_FILEVERSION 0,5,487,895
-	#define _PMATH_VERSION_RC_FILEVERSION_STRING "0, 5, 487, 895\0"
-	static const char _PMATH_VERSION_FULLVERSION_STRING [] = "0.5.487.895";
+	static const long _PMATH_VERSION_BUILDS_COUNT  = 591;
+	#define _PMATH_VERSION_RC_FILEVERSION 0,5,489,897
+	#define _PMATH_VERSION_RC_FILEVERSION_STRING "0, 5, 489, 897\0"
+	static const char _PMATH_VERSION_FULLVERSION_STRING [] = "0.5.489.897";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long _PMATH_VERSION_BUILD_HISTORY  = 0;
