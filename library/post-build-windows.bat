516dd41ef0b5b2d06699294c725fc6a098e4bcad
Peter Frentrup <frentrup@math.hu-berlin.de>
2016-01-31 22:10:25 +0100

    upgrade to MSVC 2015 for x86 target, too. So no custom inttypes.h is needed any more

--- library/post-build-windows.bat
+++ library/post-build-windows.bat
@@ -1,24 +1,30 @@
-IF not exist depencies\windows\%2\ (
-	MKDIR depencies\windows\%2
+REM Parameter %1 is the target output directory, relative to the project directory (where this 
+REM              file batch lives). 
+REM              It is assumed to be a path of *three* directories, so "%1\..\..\.." is equivalent 
+REM              to ".". This is needed for maininit.5.txt to refer to the correct file.
+REM Parameter %2 is the target architecture. It can be x86 or x64.
+
+IF not exist dependencies\windows\%2\ (
+	MKDIR dependencies\windows\%2
 )
-IF not exist console\depencies\windows\%2\ (
-	MKDIR console\depencies\windows\%2
+IF not exist console\dependencies\windows\%2\ (
+	MKDIR console\dependencies\windows\%2
 )
 
-MOVE /Y bin\windows\%1\libpmath.dll.def  bin\windows\%1\pmath.def
+MOVE /Y %1\libpmath.dll.def  %1\pmath.def
 
-MKDIR test\bin\windows\%1
-COPY /Y depencies\windows\%2\*           test\bin\windows\%1\*
-COPY /Y bin\windows\%1\*.dll        test\bin\windows\%1\*
-COPY /Y bin\windows\%1\*.pdb        test\bin\windows\%1\*
-COPY /Y scripts\maininit.5.txt           test\bin\windows\%1\maininit.pmath
+MKDIR test\%1
+COPY /Y dependencies\windows\%2\*        test\%1\*
+COPY /Y %1\*.dll                         test\%1\*
+COPY /Y %1\*.pdb                         test\%1\*
+COPY /Y scripts\maininit.5.txt           test\%1\maininit.pmath
 
-MKDIR console\bin\windows\%1
-COPY /Y console\depencies\windows\%2\*   console\bin\windows\%1\*
-COPY /Y depencies\windows\%2\*           console\bin\windows\%1\*
-COPY /Y bin\windows\%1\*.dll        console\bin\windows\%1\*
-COPY /Y bin\windows\%1\*.pdb        console\bin\windows\%1\*
-COPY /Y scripts\maininit.5.txt           console\bin\windows\%1\maininit.pmath
+MKDIR console\%1
+COPY /Y console\dependencies\windows\%2\*   console\%1\*
+COPY /Y dependencies\windows\%2\*           console\%1\*
+COPY /Y %1\*.dll                            console\%1\*
+COPY /Y %1\*.pdb                            console\%1\*
+COPY /Y scripts\maininit.5.txt              console\%1\maininit.pmath
 
 rem Only generate pmath.lib if we did a gcc build:
-IF EXIST bin\windows\%1\pmath.def  LIB /DEF:bin\windows\%1\pmath.def /OUT:bin\windows\%1\pmath.lib
+IF EXIST %1\pmath.def  LIB /DEF:%1\pmath.def /OUT:%1\pmath.lib
