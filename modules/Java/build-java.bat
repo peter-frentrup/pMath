fd64318e89d65bd5e9fae08cf460e34dcfc8ce5e
Peter Frentrup <frentrup@math.hu-berlin.de>
2016-03-26 01:44:42 +0100

    pmath-java: batch-script (windows) to compile *.java files

---
+++ modules/Java/build-java.bat
@@ -0,0 +1,38 @@
+@echo off
+
+setlocal ENABLEEXTENSIONS  ENABLEDELAYEDEXPANSION
+
+rem set "JAVA_REG_KEY=HKLM\Software\JavaSoft\Java Runtime Environment"
+set "JAVA_REG_KEY=HKLM\Software\JavaSoft\Java Development Kit"
+
+for /F "usebackq skip=2 tokens=2*" %%A in (`reg.exe query "%JAVA_REG_KEY%" /v CurrentVersion 2^>NUL`) DO (
+	set "JDK_VERSION=%%B"
+)
+if not defined JDK_VERSION (
+	echo Java Development Kit not found.
+	goto end
+)
+
+for /F "usebackq skip=2 tokens=2*" %%A in (`reg.exe query "%JAVA_REG_KEY%\%JDK_VERSION%" /v JavaHome 2^>NUL`) DO (
+	set "JDK_HOME=%%B"
+)
+if not defined JDK_HOME (
+	echo Java Development Kit %JDK_VERSION% not found.
+	goto end
+)
+
+set javac.exe="%JDK_HOME%\bin\javac.exe"
+
+pushd %~dp0
+
+%javac.exe% -source 6 -target 6 classpath\pmath\*.java
+%javac.exe% -source 6 -target 6 classpath\pmath\util\*.java
+
+%javac.exe% -source 6 -target 6 -cp classpath test\App\*.java
+%javac.exe% -source 6 -target 6 -cp classpath test\Interrupt\*.java
+
+popd
+
+:end
+pause
+endlocal
--- modules/Java/test/App/TestApp.java
+++ modules/Java/test/App/TestApp.java
@@ -1,16 +1,16 @@
 import java.io.*;
 
-public class TestApp{
+public class TestApp {
 	static class CapturedLocalValues {
 		public boolean quit;
 	}
 	
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("-Dpmath.core.base_directory = " + System.getProperty("pmath.core.base_directory"));
 		System.out.println("-Dpmath.core.dll            = " + System.getProperty("pmath.core.dll"));
 		System.out.println("-Dpmath.binding.dll         = " + System.getProperty("pmath.binding.dll"));
 		System.out.println();
-		System.out.println("pMath. Type 'QUIT' or 'Quit()' to exit.");
+		System.out.println("pMath hosted in a Java app. Type 'QUIT' or 'Quit()' to exit.");
 		
 		pmath.Core.execute("Print(`1`)", "Hello");
 		
@@ -28,18 +28,18 @@ public class TestApp{
 			"Protect(Quit);", 
 			locals);
 		
-		while(!locals.quit){
-			console.printf("\n<  ");
+		while(!locals.quit) {
+			System.out.print("\n<  ");
 			String input = console.readLine();
 			if("QUIT".equals(input))
 				break;
 				
 			Object result = pmath.Core.execute(input);
 			if(result != null){
-				console.printf(" > " + result + "\n");
+				System.out.println(" > " + result);
 			}
 		}
 		
-		console.printf("Bye.\n");
+		System.out.println("Bye.");
 	}
 }
