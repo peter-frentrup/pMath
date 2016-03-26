cdd88f29d13bbd71e6183305f4449e7cc27b2963
Peter Frentrup <frentrup@math.hu-berlin.de>
2016-03-26 15:45:17 +0100

    simplify build script

--- library/build-all.bat
+++ library/build-all.bat
@@ -12,29 +12,15 @@ for %%X in (CodeBlocks\codeblocks.exe) do set codeblocks.exe="%%~$searchPath:X"
 
 if not defined codeblocks.exe (
 	echo "CodeBlocks not found"
+	pause
 	goto end
 )
 
 pushd %~dp0
 
-for %%T in (MSVC  GCC) do (
-	for %%A in (x64  x86) do (
-		for %%D in (Debug  Release) do (
-			set target=Windows-%%T-%%D-%%A
-			<NUL set /p ="Build target !target! ... "
-			
-			%codeblocks.exe% /na /nd /ns --debug-log --target=!target! --build  pmath.cbp
-			if errorlevel 1 (
-				echo failed
-			) else (
-				echo done
-			)
-		)
-	)
-)
+%codeblocks.exe% /na /nd /ns --debug-log --no-batch-window-close --target=All-Windows --build  pmath.cbp
 
 popd
 
 :end
-pause
 endlocal
