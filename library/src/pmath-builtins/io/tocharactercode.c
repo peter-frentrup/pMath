47ebfe86474ba62882bd690c3f7bcebf55de4948
Peter Frentrup <frentrup@math.hu-berlin.de>
2016-07-12 23:43:17 +0200

    ToCharacterCode returns a packed array

--- library/src/pmath-builtins/io/tocharactercode.c
+++ library/src/pmath-builtins/io/tocharactercode.c
@@ -1,3 +1,5 @@
+#include <pmath-core/packed-arrays.h>
+
 #include <pmath-util/evaluation.h>
 #include <pmath-util/helpers.h>
 #include <pmath-util/messages.h>
@@ -5,14 +7,21 @@
 #include <pmath-builtins/all-symbols-private.h>
 
 
-static pmath_expr_t string_to_cc(pmath_string_t str) { // str will be freed
+static pmath_expr_t string_to_utf16_codes(pmath_string_t str) { // str will be freed
   const uint16_t *buf = pmath_string_buffer(&str);
   int             len = pmath_string_length(str);
   int i;
-  pmath_expr_t result = pmath_expr_new(pmath_ref(PMATH_SYMBOL_LIST), (size_t)len);
+  size_t size = (size_t)len;
+  pmath_packed_array_t result = pmath_packed_array_new(PMATH_NULL, PMATH_PACKED_INT32, 1, &size, NULL, 0);
+  int32_t *data = pmath_packed_array_begin_write(&result, NULL, 0);
+  if(!data) {
+    pmath_unref(str);
+    pmath_unref(result);
+    return PMATH_NULL;
+  }
   
   for(i = 0; i < len; ++i) {
-    result = pmath_expr_set_item(result, (size_t)i + 1, PMATH_FROM_INT32(buf[i]));
+    data[i] = (int32_t)buf[i];
   }
   
   pmath_unref(str);
@@ -21,6 +30,8 @@ static pmath_expr_t string_to_cc(pmath_string_t str) { // str will be freed
 
 
 PMATH_PRIVATE pmath_t builtin_tocharactercode(pmath_expr_t expr) {
+/* ToCharacterCode(string)
+ */
   pmath_t code;
   
   if(pmath_expr_length(expr) != 1) {
@@ -31,7 +42,7 @@ PMATH_PRIVATE pmath_t builtin_tocharactercode(pmath_expr_t expr) {
   code = pmath_expr_get_item(expr, 1);
   if(pmath_is_string(code)) {
     pmath_unref(expr);
-    return string_to_cc(code);
+    return string_to_utf16_codes(code);
   }
   
   if(pmath_is_expr_of(code, PMATH_SYMBOL_LIST)) {
