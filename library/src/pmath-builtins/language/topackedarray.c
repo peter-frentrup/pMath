d66ca2d8a24e2ef706648d2f6d30d7df7c947b7f
Peter Frentrup <frentrup@math.hu-berlin.de>
2016-07-12 23:32:17 +0200

    publish _pmath_expr_pack_array() as pmath_to_packed_array() and slightly change its type conversion rules
    Developer`ToPackedArray({1.0,2.0,3.0},Integer) will not create a Real packed array any more

--- library/src/pmath-builtins/language/topackedarray.c
+++ library/src/pmath-builtins/language/topackedarray.c
@@ -13,7 +13,7 @@ PMATH_PRIVATE pmath_t builtin_developer_topackedarray(pmath_expr_t expr) {
          specifying tolerance setting for conversion
  */
   pmath_t list;
-  pmath_packed_type_t expected_type = PMATH_PACKED_INT32;
+  pmath_packed_type_t expected_type = 0;
   
   if(pmath_expr_length(expr) < 1 || pmath_expr_length(expr) > 2){
     pmath_message_argxxx(pmath_expr_length(expr), 1, 2);
@@ -24,7 +24,9 @@ PMATH_PRIVATE pmath_t builtin_developer_topackedarray(pmath_expr_t expr) {
     pmath_t type = pmath_expr_get_item(expr, 2);
     pmath_unref(type);
     
-    if(pmath_same(type, PMATH_SYMBOL_INTEGER))
+    if(pmath_same(type, PMATH_SYMBOL_AUTOMATIC))
+      expected_type = 0;
+    else if(pmath_same(type, PMATH_SYMBOL_INTEGER))
       expected_type = PMATH_PACKED_INT32;
     else if(pmath_same(type, PMATH_SYMBOL_REAL))
       expected_type = PMATH_PACKED_DOUBLE;
@@ -35,10 +37,5 @@ PMATH_PRIVATE pmath_t builtin_developer_topackedarray(pmath_expr_t expr) {
   list = pmath_expr_get_item(expr, 1);
   pmath_unref(expr);
   
-  if(!pmath_is_expr(list))
-    return list;
-  
-  list = _pmath_expr_pack_array(list, expected_type);
-  
-  return list;
+  return pmath_to_packed_array(list, expected_type);
 }
