eb5ecbb5cbf242adba022bae01116ec527c778a5
Peter Frentrup <frentrup@math.hu-berlin.de>
2016-07-13 03:33:40 +0200

    support all available encodings in ToCharacterCode()

--- library/src/pmath-builtins/io/tocharactercode.c
+++ library/src/pmath-builtins/io/tocharactercode.c
@@ -1,13 +1,19 @@
+#include <pmath-core/expressions-private.h>
 #include <pmath-core/packed-arrays.h>
 
+#include <pmath-util/concurrency/threads.h>
 #include <pmath-util/evaluation.h>
 #include <pmath-util/helpers.h>
+#include <pmath-util/memory.h>
 #include <pmath-util/messages.h>
 
 #include <pmath-builtins/all-symbols-private.h>
 
+#include <errno.h>
+#include <iconv.h>
 
-static pmath_expr_t string_to_utf16_codes(pmath_string_t str) { // str will be freed
+
+static pmath_expr_t string_to_utf16_codes(pmath_string_t str, iconv_t dummy) { // str will be freed
   const uint16_t *buf = pmath_string_buffer(&str);
   int             len = pmath_string_length(str);
   int i;
@@ -28,7 +34,7 @@ static pmath_expr_t string_to_utf16_codes(pmath_string_t str) { // str will be f
   return result;
 }
 
-static pmath_expr_t string_to_utf32_codes(pmath_string_t str) { // str will be freed
+static pmath_expr_t string_to_utf32_codes(pmath_string_t str, iconv_t dummy) { // str will be freed
   const uint16_t *buf = pmath_string_buffer(&str);
   int             len = pmath_string_length(str);
   size_t size = (size_t)len;
@@ -59,13 +65,93 @@ static pmath_expr_t string_to_utf32_codes(pmath_string_t str) { // str will be f
   return pmath_expr_get_item_range(result, 1, data - data_start);
 }
 
+static pmath_expr_t append_bytes(pmath_expr_t expr, char *bytes, size_t num_bytes) {
+  size_t index;
+  
+  if(num_bytes == 0)
+    return expr;
+    
+  if(pmath_expr_length(expr) == 0) {
+    index = 1;
+    pmath_unref(expr);
+    expr = pmath_packed_array_new(PMATH_NULL, PMATH_PACKED_INT32, 1, &num_bytes, NULL, 0);
+  }
+  else {
+    index = pmath_expr_length(expr);
+    expr = pmath_expr_resize(expr, index + num_bytes);
+    ++index;
+  }
+  
+  // we created expr, so it is either an INT32 packed array of dimension 1 or a non-packed list
+  if(pmath_is_packed_array(expr)) {
+    int32_t *data = pmath_packed_array_begin_write(&expr, &index, 1);
+    if(!data) { // out of memory
+      pmath_unref(expr);
+      return PMATH_NULL;
+    }
+    
+    while(num_bytes-- > 0) {
+      *data++ = (int32_t)(unsigned char)(*bytes++);
+    }
+  }
+  else {
+    for(; num_bytes > 0; --num_bytes, ++index, ++bytes) {
+      expr = pmath_expr_set_item(expr, index, PMATH_FROM_INT32((int32_t)(unsigned char)(*bytes)));
+    }
+  }
+  
+  return expr;
+}
+
+static pmath_expr_t string_to_iconv_bytes(pmath_string_t str, iconv_t cd) {
+  pmath_expr_t result = pmath_ref(_pmath_object_emptylist);
+  int             len = pmath_string_length(str);
+  size_t inbytesleft;
+  char *inbuf;
+  char buffer[128];
+  
+  inbytesleft = sizeof(uint16_t) * (size_t)len;
+  inbuf  = (char *)pmath_string_buffer(&str);
+  
+  while(inbytesleft > 0) {
+    char *outbuf = buffer;
+    size_t outbytesleft = sizeof(buffer);
+    
+    size_t ret = iconv(cd, &inbuf, &inbytesleft, &outbuf, &outbytesleft);
+    result = append_bytes(result, buffer, outbuf - buffer);
+    
+    if(ret == (size_t) - 1) {
+      if(errno == EILSEQ) { // invalid input byte
+        result = pmath_expr_append(result, 1, pmath_ref(PMATH_SYMBOL_NONE));
+        
+        ++inbuf;
+        --inbytesleft;
+      }
+      else if(errno == EINVAL) { // incomplete input
+        result = pmath_expr_append(result, 1, pmath_ref(PMATH_SYMBOL_NONE));
+        break;
+      }
+      else if(errno == E2BIG) { // output buffer too small
+        // already flushed buffer
+        
+        if(pmath_aborting())
+          break;
+      }
+    }
+  }
+  
+  pmath_unref(str);
+  return result;
+}
+
 PMATH_PRIVATE pmath_t builtin_tocharactercode(pmath_expr_t expr) {
   /* ToCharacterCode(string)
      ToCharacterCode(string, encoding)
    */
   pmath_t code;
   size_t exprlen;
-  pmath_expr_t (*converter)(pmath_string_t) = string_to_utf16_codes;
+  pmath_expr_t (*converter)(pmath_string_t, iconv_t) = string_to_utf16_codes;
+  iconv_t cd = NULL;
   
   exprlen = pmath_expr_length(expr);
   if(exprlen < 1 || exprlen > 2) {
@@ -85,6 +171,14 @@ PMATH_PRIVATE pmath_t builtin_tocharactercode(pmath_expr_t expr) {
       {
         converter = string_to_utf32_codes;
       }
+      else {
+        char *to_code = pmath_string_to_utf8(code, NULL);
+        cd = iconv_open(to_code, PMATH_BYTE_ORDER < 0 ? "UTF-16LE" : "UTF-16BE");
+        if(cd)
+          converter = string_to_iconv_bytes;
+        
+        pmath_mem_free(to_code);
+      }
     }
     
     if(!converter) {
@@ -97,9 +191,15 @@ PMATH_PRIVATE pmath_t builtin_tocharactercode(pmath_expr_t expr) {
   code = pmath_expr_get_item(expr, 1);
   if(pmath_is_string(code)) {
     pmath_unref(expr);
-    return converter(code);
+    expr = converter(code, cd);
+    if(cd)
+      iconv_close(cd);
+    return expr;
   }
   
+  if(cd)
+    iconv_close(cd);
+    
   if(pmath_is_expr_of(code, PMATH_SYMBOL_LIST)) {
     size_t i;
     for(i = pmath_expr_length(code); i > 0; --i) {
