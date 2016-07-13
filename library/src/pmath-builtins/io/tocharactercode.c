dadb6f90d57532e8df9b73eaa1bc1c9ec8d207c5
Peter Frentrup <frentrup@math.hu-berlin.de>
2016-07-13 02:09:44 +0200

    add encoding parameter to ToCharacterCode(): "UTF-16" (default) or "UTF-32"/"Unicode"

--- library/src/pmath-builtins/io/tocharactercode.c
+++ library/src/pmath-builtins/io/tocharactercode.c
@@ -28,21 +28,76 @@ static pmath_expr_t string_to_utf16_codes(pmath_string_t str) { // str will be f
   return result;
 }
 
+static pmath_expr_t string_to_utf32_codes(pmath_string_t str) { // str will be freed
+  const uint16_t *buf = pmath_string_buffer(&str);
+  int             len = pmath_string_length(str);
+  size_t size = (size_t)len;
+  pmath_packed_array_t result = pmath_packed_array_new(PMATH_NULL, PMATH_PACKED_INT32, 1, &size, NULL, 0);
+  int32_t *data_start = pmath_packed_array_begin_write(&result, NULL, 0);
+  int32_t *data = data_start;
+  if(!data_start) {
+    pmath_unref(str);
+    pmath_unref(result);
+    return PMATH_NULL;
+  }
+  
+  while(len-- > 0) {
+    if( len > 0 &&
+        (buf[0] & 0xFC00) == 0xD800 &&
+        (buf[1] & 0xFC00) == 0xDC00)
+    {
+      *data++ = 0x10000 + ((((int32_t)buf[0] & 0x03FF) << 10) | (buf[1] & 0x03FF));
+      buf += 2;
+      --len;
+    }
+    else {
+      *data++ = (int32_t)(*buf++);
+    }
+  }
+  
+  pmath_unref(str);
+  return pmath_expr_get_item_range(result, 1, data - data_start);
+}
 
 PMATH_PRIVATE pmath_t builtin_tocharactercode(pmath_expr_t expr) {
-/* ToCharacterCode(string)
- */
+  /* ToCharacterCode(string)
+     ToCharacterCode(string, encoding)
+   */
   pmath_t code;
+  size_t exprlen;
+  pmath_expr_t (*converter)(pmath_string_t) = string_to_utf16_codes;
   
-  if(pmath_expr_length(expr) != 1) {
-    pmath_message_argxxx(pmath_expr_length(expr), 1, 1);
+  exprlen = pmath_expr_length(expr);
+  if(exprlen < 1 || exprlen > 2) {
+    pmath_message_argxxx(exprlen, 1, 2);
     return expr;
+  }
+  
+  if(exprlen > 1) {
+    converter = NULL;
+    code = pmath_expr_get_item(expr, 2);
+    if(pmath_is_string(code)) {
+      if(pmath_string_equals_latin1(code, "UTF-16")) {
+        converter = string_to_utf16_codes;
+      }
+      else if(pmath_string_equals_latin1(code, "UTF-32") ||
+              pmath_string_equals_latin1(code, "Unicode"))
+      {
+        converter = string_to_utf32_codes;
+      }
+    }
+    
+    if(!converter) {
+      pmath_message(PMATH_NULL, "charcode", 1, code);
+      return expr;
+    }
+    pmath_unref(code);
   }
   
   code = pmath_expr_get_item(expr, 1);
   if(pmath_is_string(code)) {
     pmath_unref(expr);
-    return string_to_utf16_codes(code);
+    return converter(code);
   }
   
   if(pmath_is_expr_of(code, PMATH_SYMBOL_LIST)) {
