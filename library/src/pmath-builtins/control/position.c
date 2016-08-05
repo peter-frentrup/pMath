3c615f388f5b2cf231b9d54ea3a849b78e0c4a11
Peter Frentrup <frentrup@math.hu-berlin.de>
2016-08-05 13:46:14 +0200

    fix Position() to support e.g. packed arrays

--- library/src/pmath-builtins/control/position.c
+++ library/src/pmath-builtins/control/position.c
@@ -18,13 +18,13 @@
 
 struct index_t {
   struct index_t *next;
-
+  
   size_t i;
 };
 
 struct index_lists_t {
   struct index_lists_t *next;
-
+  
   struct index_t       *list;
 };
 
@@ -54,20 +54,95 @@ static void destroy_index_lists(struct index_lists_t *ids) {
 
 static pmath_bool_t prepend_index(struct index_lists_t *lists, size_t i) {
   struct index_t *new_ind;
-
+  
   while(lists) {
     new_ind = pmath_mem_alloc(sizeof(struct index_t));
     if(!new_ind)
       return FALSE;
-
+      
     new_ind->next = lists->list;
     new_ind->i    = i;
     lists->list   = new_ind;
-
+    
     lists = lists->next;
   }
-
+  
   return TRUE;
+}
+
+static struct index_lists_t **collect_position(
+  struct position_info_t  *info,
+  struct index_lists_t   **list_end,
+  pmath_t                  expr, // wont be freed
+  long                     level,
+  pmath_bool_t           (*test)(pmath_t, void *),
+  void                    *test_context);
+
+static struct index_lists_t **collect_expr_items_position_slow(
+  struct position_info_t  *info,
+  struct index_lists_t   **list_end,
+  pmath_expr_t             expr, // wont be freed
+  long                     level,
+  pmath_bool_t           (*test)(pmath_t, void *),
+  void                    *test_context
+) {
+  size_t i, len;
+  
+  assert(pmath_is_expr(expr));
+  assert(info->levelmax < 0 || level < info->levelmax);
+  
+  len = pmath_expr_length(expr);
+  
+  for(i = 1; i <= len; ++i) {
+    struct index_lists_t **new_end;
+    pmath_t item = pmath_expr_get_item(expr, i);
+    
+    new_end = collect_position(
+                info,
+                list_end,
+                item,
+                level + 1,
+                test,
+                test_context);
+                
+    pmath_unref(item);
+    prepend_index(*list_end, i);
+    list_end = new_end;
+  }
+  
+  return list_end;
+}
+
+static struct index_lists_t **collect_general_expr_items_position(
+  struct position_info_t  *info,
+  struct index_lists_t   **list_end,
+  const pmath_t           *expr_items,
+  size_t                   length,
+  long                     level,
+  pmath_bool_t           (*test)(pmath_t, void *),
+  void                    *test_context
+) {
+  size_t i0;
+  
+  assert(expr_items);
+  assert(info->levelmax < 0 || level < info->levelmax);
+  
+  for(i0 = 0; i0 < length; ++i0) {
+    struct index_lists_t **new_end;
+    
+    new_end = collect_position(
+                info,
+                list_end,
+                expr_items[i0],
+                level + 1,
+                test,
+                test_context);
+                
+    prepend_index(*list_end, i0 + 1);
+    list_end = new_end;
+  }
+  
+  return list_end;
 }
 
 static struct index_lists_t **collect_position(
@@ -79,24 +154,24 @@ static struct index_lists_t **collect_position(
   void                    *test_context
 ) {
   int reldepth;
-
+  
   if(info->max == 0 || pmath_aborting())
     return list_end;
-
+    
   reldepth = _pmath_object_in_levelspec(
                expr, info->levelmin, info->levelmax, level);
-
+               
   if(reldepth > 0)
     return list_end;
-
+    
   if(pmath_is_expr(expr)) {
     if(info->levelmax < 0 || level < info->levelmax) {
       const pmath_t *items;
       struct index_lists_t **new_end;
-
+      
       if(info->with_heads) {
         pmath_t head = pmath_expr_get_item(expr, 0);
-
+        
         new_end = collect_position(
                     info,
                     list_end,
@@ -104,66 +179,66 @@ static struct index_lists_t **collect_position(
                     level + 1,
                     test,
                     test_context);
-
+                    
         pmath_unref(head);
         prepend_index(*list_end, 0);
         list_end = new_end;
       }
-
+      
       items = pmath_expr_read_item_data(expr);
       if(items) {
-        size_t len = pmath_expr_length(expr);
-        size_t i;
-
-        for(i = 0; i < len; ++i) {
-          new_end = collect_position(
-                      info,
-                      list_end,
-                      items[i],
-                      level + 1,
-                      test,
-                      test_context);
-
-          prepend_index(*list_end, i + 1);
-          list_end = new_end;
-        }
+        list_end = collect_general_expr_items_position(
+                     info,
+                     list_end,
+                     items,
+                     pmath_expr_length(expr),
+                     level,
+                     test,
+                     test_context);
       }
-      else
-        pmath_debug_print("[pmath_expr_read_item_data() gave NULL in %s:%d]\n", __FILE__, __LINE__);
+      else {
+        list_end = collect_expr_items_position_slow(
+                     info,
+                     list_end,
+                     expr,
+                     level,
+                     test,
+                     test_context);
+      }
     }
   }
-
+  
   if(info->max == 0)
     return list_end;
-
+    
   if(reldepth < 0)
     return list_end;
-
+    
   if((*test)(expr, test_context)) {
     struct index_lists_t *result;
-
+    
     result = pmath_mem_alloc(sizeof(struct index_lists_t));
     if(!result)
       return list_end;
-
+      
     info->max--;
     result->next = NULL;
     result->list = NULL;
     *list_end = result;
     return &result->next;
   }
-
+  
   return list_end;
 }
 
 static size_t list_length(void **ind) {
   size_t len = 0;
-
+  
   while(ind) {
     ++len;
     ind = *ind;
   }
-
+  
   return len;
 }
 
@@ -171,19 +246,19 @@ static pmath_expr_t index_to_expr(struct index_t *idx) {
   pmath_expr_t result;
   struct _pmath_expr_t *expr;
   size_t i;
-
+  
   expr = _pmath_expr_new_noinit(list_length((void **)idx));
   if(!expr)
     return PMATH_NULL;
-
+    
   expr->items[0] = pmath_ref(PMATH_SYMBOL_LIST);
   for(i = 1; idx != NULL; idx = idx->next, ++i) {
     expr->items[i] = pmath_integer_new_uiptr(idx->i);
   }
-
+  
   assert(i == expr->length + 1);
   assert(idx == NULL);
-
+  
   result = PMATH_FROM_PTR(expr);
   _pmath_expr_update(result);
   return result;
@@ -193,19 +268,19 @@ static pmath_expr_t index_lists_to_expr(struct index_lists_t *ids) {
   pmath_expr_t result;
   struct _pmath_expr_t *expr;
   size_t i;
-
+  
   expr = _pmath_expr_new_noinit(list_length((void **)ids));
   if(!expr)
     return PMATH_NULL;
-
+    
   expr->items[0] = pmath_ref(PMATH_SYMBOL_LIST);
   for(i = 1; ids != NULL; ids = ids->next, ++i) {
     expr->items[i] = index_to_expr(ids->list);
   }
-
+  
   assert(i == expr->length + 1);
   assert(ids == NULL);
-
+  
   result = PMATH_FROM_PTR(expr);
   _pmath_expr_update(result);
   return result;
@@ -214,7 +289,7 @@ static pmath_expr_t index_lists_to_expr(struct index_lists_t *ids) {
 
 static pmath_bool_t pattern_tester(pmath_t obj, void *pattern_ptr) {
   pmath_t *pattern = pattern_ptr;
-
+  
   return _pmath_pattern_match(obj, pmath_ref(*pattern), NULL);
 }
 
@@ -224,7 +299,7 @@ static pmath_expr_t position(
   pmath_t                 pattern // will be freed
 ) {
   struct index_lists_t *lists = NULL;
-
+  
   collect_position(
     info,
     &lists,
@@ -232,13 +307,13 @@ static pmath_expr_t position(
     0,
     pattern_tester,
     &pattern);
-
+    
   pmath_unref(expr);
   pmath_unref(pattern);
-
+  
   expr = index_lists_to_expr(lists);
   destroy_index_lists(lists);
-
+  
   return expr;
 }
 
@@ -246,10 +321,10 @@ PMATH_PRIVATE pmath_t builtin_position(pmath_expr_t expr) {
   /* Position(obj, pattern, levelspec, n)
      Position(obj, pattern, levelspec)  = Position(obj, pattern, levelspec, Infinity)
      Position(obj, pattern)             = Position(obj, pattern, {0, Infinity}, Infinity)
-
+  
      options:
        Heads->True
-
+  
      messages:
        General::innf
        General::level
@@ -258,12 +333,12 @@ PMATH_PRIVATE pmath_t builtin_position(pmath_expr_t expr) {
   size_t last_nonoption, exprlen = pmath_expr_length(expr);
   pmath_expr_t options;
   pmath_t obj, pattern;
-
+  
   if(exprlen < 2) {
     pmath_message_argxxx(exprlen, 2, 4);
     return expr;
   }
-
+  
   last_nonoption = 2;
   info.with_heads = TRUE;
   info.levelmin = 0;
@@ -271,13 +346,13 @@ PMATH_PRIVATE pmath_t builtin_position(pmath_expr_t expr) {
   info.max = SIZE_MAX;
   if(exprlen > 2) {
     pmath_t levels = pmath_expr_get_item(expr, 3);
-
+    
     if(_pmath_extract_levels(levels, &info.levelmin, &info.levelmax)) {
       last_nonoption = 3;
-
+      
       if(exprlen > 3) {
         obj = pmath_expr_get_item(expr, 4);
-
+        
         if( pmath_is_integer(obj) &&
             pmath_number_sign(obj) >= 0)
         {
@@ -297,7 +372,7 @@ PMATH_PRIVATE pmath_t builtin_position(pmath_expr_t expr) {
           pmath_message(PMATH_NULL, "innf", 2, PMATH_FROM_INT32(4), pmath_ref(expr));
           return expr;
         }
-
+        
         pmath_unref(obj);
       }
     }
@@ -305,18 +380,18 @@ PMATH_PRIVATE pmath_t builtin_position(pmath_expr_t expr) {
       pmath_message(PMATH_NULL, "level", 1, levels);
       return expr;
     }
-
+    
     pmath_unref(levels);
   }
-
+  
   pattern = pmath_expr_get_item(expr, 2);
-
+  
   options = pmath_options_extract(expr, last_nonoption);
   if(pmath_is_null(options)) {
     pmath_unref(pattern);
     return expr;
   }
-
+  
   obj = pmath_evaluate(pmath_option_value(PMATH_NULL, PMATH_SYMBOL_HEADS, options));
   if(pmath_same(obj, PMATH_SYMBOL_TRUE)) {
     info.with_heads = TRUE;
@@ -335,9 +410,9 @@ PMATH_PRIVATE pmath_t builtin_position(pmath_expr_t expr) {
   }
   pmath_unref(obj);
   pmath_unref(options);
-
+  
   obj = pmath_expr_get_item(expr, 1);
   pmath_unref(expr);
-
+  
   return position(&info, obj, pattern);
 }
