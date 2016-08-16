d6290e85beda0bdf41b027a77251e96d54796d14
Peter Frentrup <p.frentrup@ib-werk.de>
2016-08-16 13:04:15 +0200

    DepencyWalker has problems finding Windows CRT Runtime DLLs

--- richmath/post-build-windows.ps1
+++ richmath/post-build-windows.ps1
@@ -51,6 +51,9 @@ Function Get-SourceDlls([string] $TargetExe, [string] $DllSearchPath) {
 				Write-Verbose "Skip $($file.Name) from $dir"
 			}
 		}
+		ElseIf($module.Module -like 'api-ms-*') {
+			Write-Verbose "$($module.Status): Ignore Windows module $($module.Module)"
+		}
 		ElseIf($module.Status -eq '?') {
 			Write-Error "Module not found: $($module.Module)"
 		}
