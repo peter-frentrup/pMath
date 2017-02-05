16f4d4f32360c4e29a87990cfe58efbfae04724f
Peter Frentrup <frentrup@math.hu-berlin.de>
2017-02-05 22:43:41 +0100

    Ceiling, Floor, Round, IntegerPart: use adaptive precision with Arb

--- library/src/pmath-builtins/arithmetic/ceiling-floor-round-integerpart.c
+++ library/src/pmath-builtins/arithmetic/ceiling-floor-round-integerpart.c
@@ -1,14 +1,182 @@
 #include <pmath-core/numbers-private.h>
-#include <pmath-core/intervals-private.h>
 
 #include <pmath-util/approximate.h>
+#include <pmath-util/concurrency/threads-private.h>
 #include <pmath-util/evaluation.h>
+#include <pmath-util/helpers.h>
 #include <pmath-util/messages.h>
 
 #include <pmath-builtins/all-symbols-private.h>
 #include <pmath-builtins/arithmetic-private.h>
 #include <pmath-builtins/build-expr-private.h>
 
+
+static void _pmath_round_arf_to_nearest(arf_t result, const arf_t x) {
+  if(arf_is_int(x)) {
+    arf_set(result, x);
+    return;
+  }
+  
+  if(arf_is_int_2exp_si(x, -1)) { // x is a half integer: round to even
+    arf_floor(result, x);
+    if(!arf_is_int_2exp_si(x, 1)) { // Floor(x) is odd
+      arf_ceil(result, x);
+    }
+    return;
+  }
+  else {
+    arf_t diff;
+    arf_init(diff);
+    
+    arf_floor(result, x);
+    arf_sub(diff, x, result, ARF_PREC_EXACT, ARF_RND_NEAR);
+    if(arf_cmp_2exp_si(diff, -1) > 0)
+      arf_ceil(result, x);
+      
+    arf_clear(diff);
+  }
+}
+
+static void _pmath_integerpart_arf(arf_t result, const arf_t x) {
+  if(arf_sgn(x) > 0)
+    arf_floor(result, x);
+  else
+    arf_ceil(result, x);
+}
+
+static pmath_bool_t try_arf_to_integer(pmath_t *x, const arf_t value) {
+  if(arf_cmpabs_2exp_si(value, 1000000) < 0) {
+    fmpz_t result;
+    fmpz_init(result);
+    arf_get_fmpz(result, value, ARF_RND_NEAR);
+    pmath_unref(*x);
+    *x = _pmath_integer_from_fmpz(result);
+    fmpz_clear(result);
+    return TRUE;
+  }
+  return FALSE;
+}
+
+typedef void (*arf_func_t)(arf_t, const arf_t);
+
+static pmath_bool_t try_arb_to_integer(pmath_t *x, const arb_t value, slong precision, arf_func_t round_func) {
+  if(arb_is_exact(value)) {
+    arf_t a;
+    arf_init(a);
+    
+    round_func(a, arb_midref(value));
+    if(try_arf_to_integer(x, a)) {
+      arf_clear(a);
+      return TRUE;
+    }
+    
+    arf_clear(a);
+  }
+  else if(mag_cmp_2exp_si(arb_radref(value), -1) < 0) {
+    arf_t a;
+    arf_t b;
+    
+    arf_init(a);
+    arf_init(b);
+    arb_get_lbound_arf(a, value, precision);
+    arb_get_ubound_arf(b, value, precision);
+    round_func(a, a);
+    round_func(b, b);
+    if(arf_equal(a, b)) {
+      if(try_arf_to_integer(x, a)) {
+        arf_clear(a);
+        arf_clear(b);
+        return TRUE;
+      }
+    }
+    arf_clear(a);
+    arf_clear(b);
+  }
+  return FALSE;
+}
+
+static pmath_bool_t try_round_inexact(pmath_t *x, arf_func_t round_func) {
+  if(pmath_is_double(*x))
+    *x = _pmath_create_mp_float_from_d(PMATH_AS_DOUBLE(*x));
+    
+  if(pmath_is_mpfloat(*x))
+    return try_arb_to_integer(x, PMATH_AS_ARB(*x), PMATH_AS_ARB_WORKING_PREC(*x), round_func);
+    
+  if(pmath_is_expr_of_len(*x, PMATH_SYMBOL_COMPLEX, 2)) {
+    pmath_t re = pmath_expr_get_item(*x, 1);
+    pmath_t im = pmath_expr_get_item(*x, 2);
+    
+    if(try_round_inexact(&re, round_func) && try_round_inexact(&im, round_func)) {
+      *x = pmath_expr_set_item(*x, 1, re);
+      *x = pmath_expr_set_item(*x, 2, im);
+      return TRUE;
+    }
+    
+    pmath_unref(re);
+    pmath_unref(im);
+  }
+  
+  return FALSE;
+}
+
+static pmath_bool_t try_round_numeric(pmath_t *x, arf_func_t round_func) {
+  pmath_thread_t me = pmath_thread_get_current();
+  double precision;
+  double max_precision;
+  
+  if(!me)
+    return FALSE;
+    
+  precision = FLINT_MIN(16, me->max_extra_precision);
+  precision = FLINT_MAX(me->min_precision, precision);
+  max_precision = FLINT_MIN(me->max_precision, me->min_precision + me->max_extra_precision);
+  
+  while(!pmath_aborting()) {
+    pmath_t approx = pmath_set_precision(pmath_ref(*x), precision);
+    
+    if(try_round_inexact(&approx, round_func)) {
+      pmath_unref(*x);
+      *x = approx;
+      return TRUE;
+    }
+    
+    if(!pmath_is_float(approx) && !_pmath_is_nonreal_complex_interval_or_number(approx)) {
+      pmath_unref(approx);
+      break;
+    }
+    
+    pmath_unref(approx);
+    if(precision >= max_precision)
+      break;
+      
+    precision = FLINT_MIN(2 * precision, max_precision);
+  }
+  
+  if(precision >= max_precision) {
+    pmath_message(
+      PMATH_NULL, "meprec", 2,
+      _pmath_from_precision(me->max_extra_precision),
+      pmath_ref(*x));
+  }
+  
+  return FALSE;
+}
+
+
+
+static arf_func_t get_rounding_function(pmath_t head) {
+  if(pmath_same(head, PMATH_SYMBOL_CEILING))
+    return arf_ceil;
+    
+  if(pmath_same(head, PMATH_SYMBOL_FLOOR))
+    return arf_floor;
+    
+  if(pmath_same(head, PMATH_SYMBOL_INTEGERPART))
+    return _pmath_integerpart_arf;
+    
+  /*if(pmath_same(head, PMATH_SYMBOL_ROUND)) */
+  return _pmath_round_arf_to_nearest;
+}
 
 static mpfr_rnd_t get_rounding_mode(pmath_t head) {
   if(pmath_same(head, PMATH_SYMBOL_CEILING))
@@ -24,7 +192,7 @@ static mpfr_rnd_t get_rounding_mode(pmath_t head) {
   return MPFR_RNDN;
 }
 
-static pmath_t round_q(pmath_quotient_t x, pmath_t head) {
+static pmath_integer_t round_q(pmath_quotient_t x, pmath_t head) {
   pmath_mpint_t num, den, result;
   
   assert(pmath_is_quotient(x));
@@ -113,97 +281,6 @@ static pmath_t round_q(pmath_quotient_t x, pmath_t head) {
   return _pmath_mp_int_normalize(result);
 }
 
-static pmath_t round_d(double x, pmath_t head) {
-  pmath_mpint_t result = _pmath_create_mp_int(0);
-  
-  if(pmath_is_null(result)) {
-    pmath_unref(head);
-    return PMATH_NULL;
-  }
-  
-  if(pmath_same(head, PMATH_SYMBOL_CEILING)) {
-    // TODO: prevent overflow
-    mpz_set_d(
-      PMATH_AS_MPZ(result),
-      ceil(x));
-  }
-  else if(pmath_same(head, PMATH_SYMBOL_FLOOR)) {
-    // TODO: prevent overflow
-    mpz_set_d(
-      PMATH_AS_MPZ(result),
-      floor(x));
-  }
-  else if(pmath_same(head, PMATH_SYMBOL_INTEGERPART)) {
-    mpz_set_d(
-      PMATH_AS_MPZ(result),
-      x); // mpz_set_d truncates x
-  }
-  else { /*if(pmath_same(head, PMATH_SYMBOL_ROUND)) */
-    double f = floor(x);
-    
-    mpz_set_d(PMATH_AS_MPZ(result), f);
-    f = x - f;
-    
-    if( f > 0.5 ||
-        (f == 0.5 &&
-         mpz_odd_p(PMATH_AS_MPZ(result))))
-    {
-      mpz_add_ui(PMATH_AS_MPZ(result), PMATH_AS_MPZ(result), 1);
-    }
-  }
-  
-  pmath_unref(head);
-  return _pmath_mp_int_normalize(result);
-}
-
-static pmath_t round_f(pmath_mpfloat_t x, pmath_t head) {
-  pmath_mpint_t result = _pmath_create_mp_int(0);
-  
-  assert(pmath_is_mpfloat(x));
-  
-  if(pmath_is_null(result)) {
-    pmath_unref(x);
-    pmath_unref(head);
-    return PMATH_NULL;
-  }
-  
-  mpfr_get_z(
-    PMATH_AS_MPZ(result),
-    PMATH_AS_MP_VALUE(x),
-    get_rounding_mode(head));
-    
-  pmath_unref(x);
-  pmath_unref(head);
-  return _pmath_mp_int_normalize(result);
-}
-
-static pmath_t round_R(pmath_interval_t x, pmath_t head) {
-  pmath_interval_t result;
-  pmath_mpint_t    tmp = _pmath_create_mp_int(0);
-  mpfr_rnd_t       rnd = get_rounding_mode(head);
-  pmath_unref(head);
-  
-  assert(pmath_is_interval(x));
-  
-  result = _pmath_create_interval_for_result(x);
-  if(pmath_is_null(result) || pmath_is_null(tmp)) {
-    pmath_unref(x);
-    pmath_unref(result);
-    pmath_unref(tmp);
-    return PMATH_NULL;
-  }
-  
-  mpfr_get_z(PMATH_AS_MPZ(tmp), &PMATH_AS_MP_INTERVAL(x)->left, rnd);
-  mpfr_set_z(&PMATH_AS_MP_INTERVAL(result)->left, PMATH_AS_MPZ(tmp), MPFR_RNDD);
-  
-  mpfr_get_z(PMATH_AS_MPZ(tmp), &PMATH_AS_MP_INTERVAL(x)->right, rnd);
-  mpfr_set_z(&PMATH_AS_MP_INTERVAL(result)->right, PMATH_AS_MPZ(tmp), MPFR_RNDU);
-  
-  pmath_unref(tmp);
-  pmath_unref(x);
-  return result;
-}
-
 PMATH_PRIVATE pmath_t builtin_round_functions(pmath_expr_t expr) {
   /* Ceiling(x)           smallest integer greater than or equal to x
      Ceiling(x, a)        smallest multiple of a that is not less than x
@@ -250,42 +327,18 @@ PMATH_PRIVATE pmath_t builtin_round_functions(pmath_expr_t expr) {
       return round_q(x, head);
     }
     
-    if(pmath_is_double(x)) {
+    if(try_round_inexact(&x, get_rounding_function(head))) {
       pmath_unref(expr);
-      return round_d(PMATH_AS_DOUBLE(x), head);
-    }
-    
-    if(pmath_is_mpfloat(x)) {
-      pmath_unref(expr);
-      return round_f(x, head);
-    }
-    
-    if(pmath_is_interval(x)) {
-      pmath_unref(expr);
-      return round_R(x, head);
-    }
-    
-    { // TODO: use higher/adapted precision/interval arithmetic
-      pmath_t re, im;
       pmath_unref(head);
-      
-      //expr = pmath_expr_set_item(expr, 1, PMATH_NULL);
-      //x = pmath_approximate(x, HUGE_VAL, 2 * LOG2_10, NULL);
-      x = pmath_set_precision(x, -HUGE_VAL);
-      
-      if(pmath_is_float(x))
-        return pmath_expr_set_item(expr, 1, x);
-        
-      if( _pmath_re_im(x, &re, &im) && // frees x
-          pmath_is_float(re) &&
-          pmath_is_float(im))
-      {
-        return pmath_expr_set_item(expr, 1, x);
+      return x;
+    }
+    
+    if(pmath_is_numeric(x)) {
+      if(try_round_numeric(&x, get_rounding_function(head))) {
+        pmath_unref(expr);
+        pmath_unref(head);
+        return x;
       }
-      
-      pmath_unref(re);
-      pmath_unref(im);
-      return expr;
     }
     
     pmath_unref(head);
@@ -302,37 +355,10 @@ PMATH_PRIVATE pmath_t builtin_round_functions(pmath_expr_t expr) {
   
   if(len == 2) {
     pmath_t a, div;
-    
     a = pmath_expr_get_item(expr, 2);
-    
-//    if(_pmath_is_nonreal_complex_number(x)){
-//      pmath_t re = pmath_expr_get_item((pmath_expr_t)x, 1);
-//      pmath_t im = pmath_expr_get_item((pmath_expr_t)x, 2);
-//      pmath_unref(x);
-//      pmath_unref(expr);
-//      x = pmath_expr_new_extended(
-//        pmath_ref(PMATH_SYMBOL_COMPLEX), 2,
-//          pmath_expr_new_extended(pmath_ref(head), 2, re, pmath_ref(a)),
-//          pmath_expr_new_extended(pmath_ref(head), 2, im, pmath_ref(a)));
-//      pmath_unref(head);
-//      pmath_unref(a);
-//      return x;
-//    }
-
-    div = pmath_evaluate( // div = x/a
-            pmath_expr_new_extended(
-              pmath_ref(PMATH_SYMBOL_TIMES), 2,
-              x,
-              pmath_expr_new_extended(
-                pmath_ref(PMATH_SYMBOL_POWER), 2,
-                pmath_ref(a),
-                PMATH_FROM_INT32(-1))));
-                
+    div = pmath_evaluate(DIV(x, pmath_ref(a)));
     pmath_unref(expr);
-    return pmath_expr_new_extended( // Ceil(x/a)*a  or  Floor(x/a)*a
-             pmath_ref(PMATH_SYMBOL_TIMES), 2,
-             pmath_expr_new_extended(head, 1, div),
-             a);
+    return TIMES(pmath_expr_new_extended(head, 1, div), a);
   }
   
   pmath_unref(head);
