37cd46fc1598922f6cd6441c5779dca3bb54cbd5
Peter Frentrup <frentrup@math.hu-berlin.de>
2017-07-31 05:09:40 +0200

    add Windows`ShellExecute()

--- modules/Windows/library/src/main.c
+++ modules/Windows/library/src/main.c
@@ -4,11 +4,12 @@
 #include <stdlib.h>
 #include <pmath.h>
 
-extern pmath_t windows_SHGetKnownFolderPath(pmath_expr_t expr);
 extern pmath_t windows_GetAllKnownFolders(pmath_expr_t expr);
 extern pmath_t windows_RegGetValue(pmath_expr_t expr);
 extern pmath_t windows_RegEnumKeys(pmath_expr_t expr);
 extern pmath_t windows_RegEnumValues(pmath_expr_t expr);
+extern pmath_t windows_ShellExecute(pmath_expr_t expr);
+extern pmath_t windows_SHGetKnownFolderPath(pmath_expr_t expr);
 
 static void protect_all(pmath_symbol_t *start, size_t size) {
   pmath_symbol_t *end = start + size / sizeof(pmath_symbol_t);
@@ -25,14 +26,15 @@ static void free_all(pmath_symbol_t *start, size_t size) {
 }
 
 static struct {
-  pmath_symbol_t Windows_SHGetKnownFolderPath;
+  pmath_symbol_t Windows_Internal_ComInitializer;
+  pmath_symbol_t Windows_ComMultiThreadedAppartment;
   pmath_symbol_t Windows_Private_GetAllKnownFolders;
   pmath_symbol_t Windows_RegGetValue;
   pmath_symbol_t Windows_RegEnumKeys;
   pmath_symbol_t Windows_RegEnumValues;
+  pmath_symbol_t Windows_ShellExecute;
+  pmath_symbol_t Windows_SHGetKnownFolderPath;
   pmath_symbol_t Windows_Win64Version;
-  pmath_symbol_t Windows_ComMultiThreadedAppartment;
-  pmath_symbol_t Windows_Internal_ComInitializer;
 } symbols;
 
 const pmath_symbol_t *Windows_Win64Version = &symbols.Windows_Win64Version;
@@ -82,20 +84,22 @@ pmath_bool_t pmath_module_init(pmath_string_t filename) {
 
   memset(&symbols, 0, sizeof(symbols));
   
-  VERIFY( symbols.Windows_SHGetKnownFolderPath        = NEW_SYMBOL("Windows`SHGetKnownFolderPath"));
+  VERIFY( symbols.Windows_Internal_ComInitializer     = NEW_SYMBOL("Windows`Internal`ComInitializer"));
+  VERIFY( symbols.Windows_ComMultiThreadedAppartment  = NEW_SYMBOL("Windows`$ComMultiThreadedAppartment"));
   VERIFY( symbols.Windows_Private_GetAllKnownFolders  = NEW_SYMBOL("Windows`Private`GetAllKnownFolders"));
   VERIFY( symbols.Windows_RegGetValue                 = NEW_SYMBOL("Windows`RegGetValue"));
   VERIFY( symbols.Windows_RegEnumKeys                 = NEW_SYMBOL("Windows`RegEnumKeys"));
   VERIFY( symbols.Windows_RegEnumValues               = NEW_SYMBOL("Windows`RegEnumValues"));
+  VERIFY( symbols.Windows_ShellExecute                = NEW_SYMBOL("Windows`ShellExecute"));
+  VERIFY( symbols.Windows_SHGetKnownFolderPath        = NEW_SYMBOL("Windows`SHGetKnownFolderPath"));
   VERIFY( symbols.Windows_Win64Version                = NEW_SYMBOL("Windows`Win64Version"));
-  VERIFY( symbols.Windows_ComMultiThreadedAppartment  = NEW_SYMBOL("Windows`$ComMultiThreadedAppartment"));
-  VERIFY( symbols.Windows_Internal_ComInitializer     = NEW_SYMBOL("Windows`Internal`ComInitializer"));
   
-  BIND_DOWN(symbols.Windows_SHGetKnownFolderPath,       windows_SHGetKnownFolderPath);
   BIND_DOWN(symbols.Windows_Private_GetAllKnownFolders, windows_GetAllKnownFolders);
   BIND_DOWN(symbols.Windows_RegGetValue,                windows_RegGetValue);
   BIND_DOWN(symbols.Windows_RegEnumKeys,                windows_RegEnumKeys);
   BIND_DOWN(symbols.Windows_RegEnumValues,              windows_RegEnumValues);
+  BIND_DOWN(symbols.Windows_ShellExecute,               windows_ShellExecute);
+  BIND_DOWN(symbols.Windows_SHGetKnownFolderPath,       windows_SHGetKnownFolderPath);
   
   PMATH_RUN("Windows`$KnownFolders::= Windows`Private`GetAllKnownFolders()");
   PMATH_RUN("Options(Windows`SHGetKnownFolderPath):= {CreateDirectory->False}");
