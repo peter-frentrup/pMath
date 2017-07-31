285546358cdfe94fa497476e1eb4430a54adf837
Peter Frentrup <frentrup@math.hu-berlin.de>
2017-07-31 06:18:38 +0200

    ShellExecute: support argument *list* for parameters string

--- modules/Windows/library/src/ShellExecute.c
+++ modules/Windows/library/src/ShellExecute.c
@@ -6,6 +6,57 @@
 #include <objbase.h>
 #include <shlobj.h>
 
+// the inverse of CommandLineToArgvW
+static pmath_t try_compose_arguments(pmath_expr_t list) { // frees list; return a string or PMATH_UNDEFINED
+  pmath_string_t result = PMATH_NULL;
+  size_t i;
+  size_t listlen;
+  
+  listlen = pmath_expr_length(list);
+  for(i = 1; i <= listlen; ++i) {
+    pmath_string_t arg = pmath_expr_get_item(list, i);
+    int len;
+    const uint16_t *buf;
+    int start;
+    
+    if(!pmath_is_string(arg)) {
+      pmath_unref(arg);
+      pmath_unref(result);
+      pmath_unref(list);
+      return PMATH_UNDEFINED;
+    }
+    
+    result = pmath_string_insert_latin1(result, INT_MAX, "\"", -1);
+      
+    len = pmath_string_length(arg);
+    buf = pmath_string_buffer(&arg);
+    start = 0;
+    while(start < len) {
+      int next_quote = start;
+      while(next_quote < len && buf[next_quote] != '\"')
+        ++next_quote;
+        
+      int first_slash = next_quote;
+      while(first_slash > start && buf[first_slash - 1] == '\\')
+        --first_slash;
+      
+      result = pmath_string_insert_ucs2(result, INT_MAX, buf + start, first_slash - start);
+      result = pmath_string_insert_ucs2(result, INT_MAX, buf + first_slash, next_quote - first_slash);
+      result = pmath_string_insert_ucs2(result, INT_MAX, buf + first_slash, next_quote - first_slash);
+      if(next_quote < len)
+        result = pmath_string_insert_latin1(result, INT_MAX, "\\\"", -1);
+      
+      start = next_quote + 1;
+    }
+    
+    result = pmath_string_insert_latin1(result, INT_MAX, "\" ", -1);
+    pmath_unref(arg);
+  }
+  
+  pmath_unref(list);
+  return result;
+}
+
 
 pmath_t windows_ShellExecute(pmath_expr_t expr) {
   /* ShellExecute(verb, file)
@@ -47,6 +98,17 @@ pmath_t windows_ShellExecute(pmath_expr_t expr) {
       pmath_unref(parameters);
       parameters = PMATH_NULL;
       options = pmath_options_extract(expr, 3);
+    }
+    else if(pmath_is_expr_of(parameters, PMATH_SYMBOL_LIST)) {
+      parameters = try_compose_arguments(parameters);
+      if(pmath_is_string(parameters)) {
+        options = pmath_options_extract(expr, 3);
+      }
+      else {
+        pmath_unref(parameters);
+        parameters = PMATH_NULL;
+        options = pmath_options_extract(expr, 2);
+      }
     }
     else {
       pmath_unref(parameters);
