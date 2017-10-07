1f0cd39a119cc73f6d49436cf3d3bb35f984da81
Peter Frentrup <frentrup@math.hu-berlin.de>
2017-10-07 23:55:57 +0200

    fast-path for Cot, Sec, Csc

--- modules/Numerics/library/src/trig-impl.c
+++ modules/Numerics/library/src/trig-impl.c
@@ -3,8 +3,36 @@
 
 extern pmath_symbol_t pmath_System_Private_AutoSimplifyTrig;
 
+static void acb_csc(acb_t r, const acb_t z, slong prec) {
+  acb_sin(r, z, prec);
+  acb_inv(r, r, prec);
+}
+
+static void acb_sec(acb_t r, const acb_t z, slong prec) {
+  acb_cos(r, z, prec);
+  acb_inv(r, r, prec);
+}
+
 PMATH_PRIVATE pmath_t eval_System_Cos(pmath_expr_t expr) {
 #  define ACB_FUNCTION acb_cos
+#    include "trig-impl.inc"
+#  undef ACB_FUNCTION
+}
+
+PMATH_PRIVATE pmath_t eval_System_Cot(pmath_expr_t expr) {
+#  define ACB_FUNCTION acb_cot
+#    include "trig-impl.inc"
+#  undef ACB_FUNCTION
+}
+
+PMATH_PRIVATE pmath_t eval_System_Csc(pmath_expr_t expr) {
+#  define ACB_FUNCTION acb_csc
+#    include "trig-impl.inc"
+#  undef ACB_FUNCTION
+}
+
+PMATH_PRIVATE pmath_t eval_System_Sec(pmath_expr_t expr) {
+#  define ACB_FUNCTION acb_sec
 #    include "trig-impl.inc"
 #  undef ACB_FUNCTION
 }
