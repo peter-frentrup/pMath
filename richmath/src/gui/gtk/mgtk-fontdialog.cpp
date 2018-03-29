peter-frentrup/pMath

commit 0ab03ae07dcf45effce96908b921b5104ac85892

use auto and if(auto var = ...)

richmath/src/gui/gtk/mgtk-fontdialog.cpp
@@ -17,21 +17,15 @@ static Expr font_chooser_dialog_show(SharedPtr<Style> initial_style) {
   GtkFontChooserDialog *dialog;
   GtkFontChooser       *chooser;
 
-  GtkWindow *parent_window = 0;
+  GtkWindow *parent_window = nullptr;
   Box *box = Application::get_evaluation_box();
   if(!box)
     box = get_current_document();
     
   if(box) {
-    Document *doc = box->find_parent<Document>(true);
-
-    if(doc) {
-      MathGtkWidget *widget = dynamic_cast<MathGtkWidget *>(doc->native());
-
-      if(widget) {
-        GtkWidget *wid = widget->widget();
-
-        if(wid)
+    if(auto doc = box->find_parent<Document>(true)) {
+      if(auto widget = dynamic_cast<MathGtkWidget *>(doc->native())) {
+        if(GtkWidget *wid = widget->widget())
           parent_window = GTK_WINDOW(gtk_widget_get_ancestor(wid, GTK_TYPE_WINDOW));
       }
     }
@@ -42,10 +36,9 @@ static Expr font_chooser_dialog_show(SharedPtr<Style> initial_style) {
 
   if(initial_style) {
     PangoFontDescription *desc;
-    char                 *utf8_name = 0;
+    char                 *utf8_name = nullptr;
 
     desc = pango_font_description_new();
-    
     Expr families;
     if(initial_style->get(FontFamilies, &families)) {
       String family(families);
@@ -59,7 +52,7 @@ static Expr font_chooser_dialog_show(SharedPtr<Style> initial_style) {
         }
       }
       
-      char *utf8_name = pmath_string_to_utf8(family.get_as_string(), nullptr);
+      utf8_name = pmath_string_to_utf8(family.get_as_string(), nullptr)
       if(utf8_name)
         pango_font_description_set_family_static(desc, utf8_name);
     }
@@ -98,8 +91,7 @@ static Expr font_chooser_dialog_show(SharedPtr<Style> initial_style) {
         if(desc) {
           PangoFontMask set_fields = pango_font_description_get_set_fields(desc);
 
-          const char *utf8_name = pango_font_description_get_family(desc);
-          if(utf8_name)
+          if(const char *utf8_name = pango_font_description_get_family(desc))
             result_style->set(FontFamilies, String::FromUtf8(utf8_name));
 
           if(set_fields & PANGO_FONT_MASK_WEIGHT) {
@@ -220,8 +212,7 @@ static Expr font_selection_dialog_show(SharedPtr<Style> initial_style) {
       pango_font_description_set_weight(desc, weight >= FontWeightBold ? PANGO_WEIGHT_BOLD  : PANGO_WEIGHT_NORMAL);
     }
 
-    char *desc_str = pango_font_description_to_string(desc);
-    if(desc_str) {
+    if(char *desc_str = pango_font_description_to_string(desc)) {
       gtk_font_selection_set_font_name(widget, desc_str);
 
       g_free(desc_str);
@@ -238,34 +229,28 @@ static Expr font_selection_dialog_show(SharedPtr<Style> initial_style) {
     case GTK_RESPONSE_OK: {
         SharedPtr<Style> result_style = new Style();
 
-        char *desc_str = gtk_font_selection_dialog_get_font_name(dialog);
-        if(desc_str) {
+        if(char *desc_str = gtk_font_selection_dialog_get_font_name(dialog)) {
           PangoFontDescription *desc;
           PangoFontMask         set_fields;
 
           desc       = pango_font_description_from_string(desc_str);
           set_fields = pango_font_description_get_set_fields(desc);
 
-          const char *utf8_name = pango_font_description_get_family(desc);
-          if(utf8_name){
+          if(const char *utf8_name = pango_font_description_get_family(desc))
             result_style->set(FontFamilies, split_family_names(utf8_name));
-          }
 
           if(set_fields & PANGO_FONT_MASK_WEIGHT) {
             PangoWeight weight = pango_font_description_get_weight(desc);
-
             result_style->set(FontWeight, weight >= PANGO_WEIGHT_BOLD ? FontWeightBold  : FontWeightPlain);
           }
 
           if(set_fields & PANGO_FONT_MASK_STYLE) {
             PangoStyle style = pango_font_description_get_style(desc);
-
             result_style->set(FontSlant, style == PANGO_STYLE_NORMAL ? FontSlantPlain : FontSlantItalic);
           }
 
           if(set_fields & PANGO_FONT_MASK_SIZE) {
             int size = pango_font_description_get_size(desc);
-
             if(size > 0)
               result_style->set(FontSize, (float)size / PANGO_SCALE);
           }

richmath/src/boxes/dynamicbox.cpp
@@ -28,7 +28,7 @@ Box *AbstractDynamicBox::dynamic_to_literal(int *start, int *end) {
   int e = content()->length();
   content()->dynamic_to_literal(&s, &e);
   
-  MathSequence *seq = dynamic_cast<MathSequence*>(parent());
+  auto seq = dynamic_cast<MathSequence*>(parent());
   if(!seq)
     return this;
     

richmath/src/boxes/fractionbox.cpp
@@ -118,9 +118,7 @@ void FractionBox::paint(Context *context) {
 }
 
 Box *FractionBox::remove(int *index) {
-  MathSequence *seq = dynamic_cast<MathSequence*>(_parent);
-  
-  if(seq) {
+  if(MathSequence *seq = dynamic_cast<MathSequence*>(_parent)) {
     if(*index == 0 && _numerator->length() == 0) {
       *index = _index;
       seq->insert(_index + 1, _denominator, 0, _denominator->length());

richmath/src/boxes/graphics/graphicsbox.cpp
@@ -236,7 +236,7 @@ int GraphicsBox::count() {
 }
 
 bool GraphicsBox::expand(const BoxSize &size) {
-  MathSequence *seq = dynamic_cast<MathSequence *>(_parent);
+  auto seq = dynamic_cast<MathSequence*>(_parent);
   if(_parent && seq->length() == 1) {
     if(dynamic_cast<FillBox *>(seq->parent())) {
       BoxSize old_size = _extents;
@@ -1189,18 +1189,14 @@ Box *GraphicsBox::mouse_sensitive() {
 
 void GraphicsBox::on_mouse_enter() {
   if(error_boxes_expr.is_valid()) {
-    Document *doc = find_parent<Document>(false);
-    
-    if(doc)
+    if(auto doc = find_parent<Document>(false))
       doc->native()->show_tooltip(error_boxes_expr);
   }
 }
 
 void GraphicsBox::on_mouse_exit() {
   if(error_boxes_expr.is_valid()) {
-    Document *doc = find_parent<Document>(false);
-    
-    if(doc)
+    if(auto doc = find_parent<Document>(false))
       doc->native()->hide_tooltip();
   }
 }
@@ -1212,8 +1208,7 @@ void GraphicsBox::on_mouse_down(MouseEvent &event) {
   
   mouse_over_part = part == GraphicsPartNone ? GraphicsPartNone : GraphicsPartBackground;
   
-  Document *doc = find_parent<Document>(false);
-  if(doc) {
+  if(auto doc = find_parent<Document>(false)) {
     if(doc->selection_box() != this)
       doc->select(this, 0, 0);
     else
@@ -1308,11 +1303,9 @@ void GraphicsBox::on_mouse_move(MouseEvent &event) {
     event.set_origin(this);
     
     int part = calc_mouse_over_part(event.x, event.y);
-    
     mouse_over_part = part == GraphicsPartNone ? GraphicsPartNone : GraphicsPartBackground;
     
-    Document *doc = find_parent<Document>(false);
-    if(doc) {
+    if(auto doc = find_parent<Document>(false)) {
       if(doc->selection_box() == this) {
         mouse_over_part = part;
       }

richmath/src/boxes/graphics/graphicselement.cpp
@@ -97,16 +97,12 @@ GraphicsElement *GraphicsElement::create(Expr expr, int opts) {
   Expr head = expr[0];
   
   if(head == PMATH_SYMBOL_POINTBOX) {
-    GraphicsElement *ge = PointBox::create(expr, opts);
-    
-    if(ge)
+    if(auto ge = PointBox::create(expr, opts))
       return ge;
   }
   
   if(head == PMATH_SYMBOL_LINEBOX) {
-    GraphicsElement *ge = LineBox::create(expr, opts);
-    
-    if(ge)
+    if(auto ge = LineBox::create(expr, opts))
       return ge;
   }
   
@@ -114,20 +110,18 @@ GraphicsElement *GraphicsElement::create(Expr expr, int opts) {
       head == PMATH_SYMBOL_HUE       ||
       head == PMATH_SYMBOL_GRAYLEVEL)
   {
-    GraphicsElement *ge = ColorBox::create(expr, opts);
-    
-    if(ge)
+    if(auto ge = ColorBox::create(expr, opts))
       return ge;
   }
   
   if(head == PMATH_SYMBOL_LIST) {
-    GraphicsElementCollection *coll = new GraphicsElementCollection;
+    auto coll = new GraphicsElementCollection;
     coll->load_from_object(expr, opts);
     return coll;
   }
   
   if(head == PMATH_SYMBOL_DIRECTIVE) {
-    GraphicsDirective *dir = new GraphicsDirective;
+    auto dir = new GraphicsDirective;
     
     if(dir->try_load_from_object(expr, opts))
       return dir;

richmath/src/boxes/gridbox.cpp
@@ -695,9 +695,7 @@ Box *GridBox::remove_range(int *start, int end) {
     if(cols() == 1) {
       if(ay == 1 && by == rows() - 1) {
         *start = _index;
-        MathSequence *seq = dynamic_cast<MathSequence *>(_parent);
-        
-        if(seq) {
+        if(MathSequence *seq = dynamic_cast<MathSequence *>(_parent)) {
           MathSequence *content = items[0]->content();
           seq->insert(
             _index,
@@ -711,9 +709,7 @@ Box *GridBox::remove_range(int *start, int end) {
       }
       else if(ay == 0 && by == rows() - 2) {
         *start = _index;
-        MathSequence *seq = dynamic_cast<MathSequence *>(_parent);
-        
-        if(seq) {
+        if(MathSequence *seq = dynamic_cast<MathSequence *>(_parent)) {
           MathSequence *content = items[items.length() - 1]->content();
           seq->insert(
             _index,
@@ -734,9 +730,7 @@ Box *GridBox::remove_range(int *start, int end) {
     if(rows() == 1) {
       if(ax == 1 && bx == cols() - 1) {
         *start = _index;
-        MathSequence *seq = dynamic_cast<MathSequence *>(_parent);
-        
-        if(seq) {
+        if(MathSequence *seq = dynamic_cast<MathSequence *>(_parent)) {
           MathSequence *content = items[0]->content();
           seq->insert(
             _index,
@@ -750,9 +744,7 @@ Box *GridBox::remove_range(int *start, int end) {
       }
       else if(ax == 0 && bx == cols() - 2) {
         *start = _index;
-        MathSequence *seq = dynamic_cast<MathSequence *>(_parent);
-        
-        if(seq) {
+        if(MathSequence *seq = dynamic_cast<MathSequence *>(_parent)) {
           MathSequence *content = items[items.length() - 1]->content();
           seq->insert(
             _index,

richmath/src/boxes/inputfieldbox.cpp
@@ -158,8 +158,7 @@ void InputFieldBox::paint_content(Context *context) {
       
       if(was_parent) {
         context->selection = SelectionReference();
-        Document *doc = find_parent<Document>(false);
-        if(doc)
+        if(auto doc = find_parent<Document>(false))
           doc->select(content(), content()->length(), content()->length());
       }
       
@@ -324,8 +323,7 @@ bool InputFieldBox::selectable(int i) {
 }
 
 void InputFieldBox::on_mouse_down(MouseEvent &event) {
-  Document *doc = find_parent<Document>(false);
-  if(doc) {
+  if(auto doc = find_parent<Document>(false)) {
     if(event.left) {
       event.set_origin(0);
       float gx = event.x;
@@ -367,9 +365,7 @@ void InputFieldBox::on_mouse_down(MouseEvent &event) {
 }
 
 void InputFieldBox::on_mouse_move(MouseEvent &event) {
-  Document *doc = find_parent<Document>(false);
-  
-  if(doc) {
+  if(auto doc = find_parent<Document>(false)) {
     event.set_origin(this);
     
     int start, end;
@@ -416,8 +412,7 @@ void InputFieldBox::on_key_down(SpecialKeyEvent &event) {
         dynamic_updated();
         
       if(!assign_dynamic()) {
-        Document *doc = find_parent<Document>(false);
-        if(doc)
+        if(auto doc = find_parent<Document>(false))
           doc->native()->beep();
           
         must_update = true;

richmath/src/boxes/interpretationbox.cpp
@@ -98,9 +98,7 @@ bool InterpretationBox::edit_selection(Context *context) {
     return false;
     
   if(get_own_style(AutoDelete)) {
-    MathSequence *seq = dynamic_cast<MathSequence *>(_parent);
-    
-    if(seq) {
+    if(MathSequence *seq = dynamic_cast<MathSequence *>(_parent)) {
       int len = _content->length();
       
       if(context->selection.get() == _content) {

commit 1f7d96f73dd26609babd1b492994517f093ddc9f

fix syntax error introduced in commit 0ab03ae07... "use auto and if(auto var = ...)"
Oops, the Gtk3 version was obviously not tested for a long time ^.^

richmath/src/gui/gtk/mgtk-fontdialog.cpp
@@ -52,7 +52,7 @@ static Expr font_chooser_dialog_show(SharedPtr<Style> initial_style) {
         }
       }
       
-      utf8_name = pmath_string_to_utf8(family.get_as_string(), nullptr)
+      utf8_name = pmath_string_to_utf8(family.get_as_string(), nullptr);
       if(utf8_name)
         pango_font_description_set_family_static(desc, utf8_name);
     }