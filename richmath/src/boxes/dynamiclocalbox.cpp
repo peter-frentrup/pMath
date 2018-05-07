peter-frentrup/pMath

commit 85d3877843c730957915c0675a626bd7817ce5d8

range-based for loops for pmath::Expr

richmath/src/boxes/dynamiclocalbox.cpp
@@ -39,9 +39,7 @@ bool DynamicLocalBox::try_load_from_object(Expr expr, BoxInputFlags options) {
   if(symbols[0] != PMATH_SYMBOL_LIST)
     return false;
     
-  for(size_t i = symbols.expr_length(); i > 0; --i) {
-    Expr sym = symbols[i];
-    
+  for(auto sym : symbols.items()) {
     if(!sym.is_symbol())
       return false;
   }
@@ -97,14 +95,12 @@ void DynamicLocalBox::paint(Context *context) {
 
 static pmath_t internal_replace_symbols(pmath_t expr, const Expr &old_syms, const Expr &new_syms) {
   if(pmath_is_symbol(expr)) {
-    size_t i;
-    for(i = old_syms.expr_length(); i > 0; --i) {
-      if(old_syms[i] == expr) {
+    for(Expr item : old_syms.items_reverse()) {
+      if(item == expr) {
         pmath_unref(expr);
-        return new_syms[i].release();
+        return item.release();
       }
     }
-    
     return expr;
   }
   
@@ -139,11 +135,9 @@ Expr DynamicLocalBox::to_pmath(BoxOutputFlags flags) {
   {
     Gather g2;
     
-    for(size_t i = 1; i <= _public_symbols.expr_length(); ++i) {
-      Expr sym = _public_symbols[i];
-      
-      for(size_t j = _unsaved_variables.expr_length(); j > 0; --j) {
-        if(_unsaved_variables[j] == sym) {
+    for(auto sym : _public_symbols.items()) {
+      for(auto unsaved : _unsaved_variables.items_reverse()) {
+        if(unsaved == sym) {
           sym = Expr();
           break;
         }

library/include/pmath-cpp.h
@@ -49,6 +49,108 @@ namespace pmath {
    */
   class Expr {
     public:
+      class ItemsEnum;
+      class ReverseItemsEnum;
+      
+      class Iterator {
+          friend class ItemsEnum;
+        public:
+          bool operator!=(const Iterator &other) const throw() {
+            return _expr_ptr != other._expr_ptr || _index != other._index;
+          }
+          Expr operator*() const {
+            return (*_expr_ptr)[_index];
+          }
+          const Iterator &operator++() {
+            ++_index;
+            return *this;
+          }
+          
+        private:
+          Iterator(const Expr *expr_ptr, const size_t index)
+            : _expr_ptr(expr_ptr), 
+              _index(index)
+          {
+          }
+          
+        private:
+          const Expr *_expr_ptr;
+          size_t      _index;
+      };
+      
+      class ReverseIterator {
+          friend class ReverseItemsEnum;
+        public:
+          bool operator!=(const ReverseIterator &other) const throw() {
+            return _expr_ptr != other._expr_ptr || _index_plus_1 != other._index_plus_1;
+          }
+          Expr operator*() const {
+            return (*_expr_ptr)[_index_plus_1 - 1];
+          }
+          const ReverseIterator &operator++() {
+            --_index_plus_1;
+            return *this;
+          }
+          
+        private:
+          ReverseIterator(const Expr *expr_ptr, const size_t index_plus_1)
+            : _expr_ptr(expr_ptr), 
+              _index_plus_1(index_plus_1)
+          {
+          }
+          
+        private:
+          const Expr *_expr_ptr;
+          size_t      _index_plus_1;
+      };
+      
+      class ItemsEnum {
+        public:
+          ItemsEnum(const Expr *expr_ptr, size_t first_index, size_t last_index) 
+            : _expr_ptr(expr_ptr),
+              _first_index(first_index),
+              _last_index(last_index < first_index ? first_index - 1 : last_index)
+          {
+          }
+          
+          Iterator begin() const throw() {
+            return Iterator(_expr_ptr, _first_index);
+          }
+          
+          Iterator end() const throw() {
+            return Iterator(_expr_ptr, _last_index + 1);
+          }
+        
+        private:
+          const Expr *_expr_ptr;
+          const size_t _first_index;
+          const size_t _last_index;
+      };
+      
+      class ReverseItemsEnum {
+        public:
+          ReverseItemsEnum(const Expr *expr_ptr, size_t smallest_index, size_t largest_index) 
+            : _expr_ptr(expr_ptr),
+              _smallest_index(smallest_index),
+              _largest_index(largest_index < smallest_index ? smallest_index - 1 : largest_index)
+          {
+          }
+          
+          ReverseIterator begin() const throw() {
+            return ReverseIterator(_expr_ptr, _largest_index + 1);
+          }
+          
+          ReverseIterator end() const throw() {
+            return ReverseIterator(_expr_ptr, _smallest_index);
+          }
+        
+        private:
+          const Expr *_expr_ptr;
+          const size_t _smallest_index;
+          const size_t _largest_index;
+      };
+      
+    public:
       /**\brief Initialize with PMATH_NULL */
       Expr() throw()
         : _obj(PMATH_NULL)
@@ -68,7 +170,7 @@ namespace pmath {
       }
       
 #ifdef PMATH_CPP_USE_RVALUE_REF
-      Expr(Expr  &&src) throw()
+      Expr(Expr &&src) throw()
         : _obj(src._obj)
       {
         src._obj = PMATH_NULL;
@@ -235,6 +337,34 @@ namespace pmath {
         return Expr(pmath_expr_get_item(_obj, (size_t)i));
       }
       
+      /**\brief Enumerate all items [1..length()] with C++ range based for loops
+       */
+      ItemsEnum items() const throw() {
+        return items((size_t)1, expr_length());
+      }
+      
+      /**\brief Enumerate some items with C++ range based for loops.
+         \param first Index of the first item, default is 1.
+         \param last  Index of the last item (inclusive), default is expr_length().
+       */
+      ItemsEnum items(size_t first, size_t last) const throw() {
+        return ItemsEnum(this, first, last);
+      }
+      
+      /**\brief Enumerate all items in reverse order with C++ range based for loops
+       */
+      ReverseItemsEnum items_reverse() const throw() {
+        return items_reverse((size_t)1, expr_length());
+      }
+      
+      /**\brief Enumerate some items in reverse with C++ range based for loops.
+         \param smallest Index of the first item (returned last), default is 1.
+         \param largest  Index of the last item (returned first), default is expr_length().
+       */
+      ReverseItemsEnum items_reverse(size_t smallest, size_t largest) const throw() {
+        return ReverseItemsEnum(this, smallest, largest);
+      }
+      
       /**\brief Change the i-th argument of an expression
          \param i Index. May be > expr_length().
          \param e The new element.
@@ -325,7 +455,7 @@ namespace pmath {
         pmath_t approx = pmath_set_precision(
                            pmath_ref(_obj),
                            -::std::numeric_limits<double>::infinity());
-        
+                           
         if(pmath_is_number(approx))
           def = pmath_number_get_d(approx);
           

richmath/src/main.cpp
@@ -102,8 +102,7 @@ static void load_aliases(
 #endif
   
   if(aliases[0] == PMATH_SYMBOL_LIST) {
-    for(size_t i = 1; i <= aliases.expr_length(); ++i) {
-      Expr rule = aliases[i];
+    for(auto rule : aliases.items()) {
       if(rule.is_rule()) {
         String lhs = rule[1];
         Expr   rhs = rule[2];
@@ -192,15 +191,12 @@ static void load_fonts() {
   Expr font_files = Evaluate(Parse("FE`$PrivateStartupFontFiles"));
   
   if(font_files[0] == PMATH_SYMBOL_LIST) {
-    for(size_t i = 1;i <= font_files.expr_length();++i) {
-      String filename(font_files[i]);
-      
-      if(FontInfo::add_private_font(filename)) {
-        pmath_debug_print_object("add private font ", filename.get(), "\n");
+    for(auto item : font_files.items()) {
+      if(FontInfo::add_private_font(String { item })) {
+        pmath_debug_print_object("add private font ", item.get(), "\n");
       }
       else {
-        Expr arg = font_files[i];
-        pmath_debug_print_object("failed to add private font ", arg.get(), "\n");
+        pmath_debug_print_object("failed to add private font ", item.get(), "\n");
       }
     }
   }
@@ -221,9 +217,7 @@ static void load_math_shapers() {
   SharedPtr<MathShaper> def;
   
   if(prefered_fonts[0] == PMATH_SYMBOL_LIST) {
-    for(size_t i = 1; i <= prefered_fonts.expr_length(); ++i) {
-      String s(prefered_fonts[i]);
-      
+    for(String s : prefered_fonts.items()) {
       shaper = MathShaper::available_shapers[s];
       
       if(!shaper) {

richmath/src/util/style.cpp
@@ -91,8 +91,8 @@ int richmath::pmath_to_color(Expr obj) {
       }
       
       if(obj.expr_length() >= 1 && obj.expr_length() <= 3) {
-        for(int i = obj.expr_length(); i > 0; --i)
-          if(!obj[i].is_number())
+        for(auto item : obj.items())
+          if(!item.is_number())
             return -1;
             
         double h, s = 1, v = 1;

commit 98594a3efa70745dba1d913083f433d6c7bda3d2

fix bug in DynamicLocalBox::prepare_dynamic(), introduced in 85d3877 (range-based for loops for pmath::Expr)

richmath/src/boxes/dynamiclocalbox.cpp
@@ -95,18 +95,17 @@ void DynamicLocalBox::paint(Context *context) {
 
 static pmath_t internal_replace_symbols(pmath_t expr, const Expr &old_syms, const Expr &new_syms) {
   if(pmath_is_symbol(expr)) {
-    for(Expr item : old_syms.items_reverse()) {
-      if(item == expr) {
+    for(size_t i = old_syms.expr_length();i > 0;--i) {
+      if(old_syms[i] == expr) {
         pmath_unref(expr);
-        return item.release();
+        return new_syms[i].release();
       }
     }
     return expr;
   }
   
   if(pmath_is_expr(expr)) {
-    size_t i;
-    for(i = 0; i <= pmath_expr_length(expr); ++i) {
+    for(size_t i = 0; i <= pmath_expr_length(expr); ++i) {
       pmath_t item = pmath_expr_extract_item(expr, i);
       
       item = internal_replace_symbols(item, old_syms, new_syms);