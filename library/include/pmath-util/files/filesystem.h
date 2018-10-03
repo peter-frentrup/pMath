3b13995957ea3ab3c4a148b72f4a9fb06a5c366f
Peter Frentrup <frentrup@math.hu-berlin.de>
2018-10-03 18:06:50 +0200

    clarify, that pmath_to_absolute_file_name() does no canonicalization

--- library/include/pmath-util/files/filesystem.h
+++ library/include/pmath-util/files/filesystem.h
@@ -6,6 +6,8 @@
 /** \brief Convert an absolute or relative file path to an absolute file path if possible.
     \param relname The relative or absolute path. It will be freed.
     \return An absolute file path or PMATH_NULL. You must free it.
+    
+    This function does not attempt to canonicalize the resulting path.
  */
 PMATH_API
 PMATH_ATTRIBUTE_USE_RESULT
