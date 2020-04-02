ec77a9dc5b4cc6d8f936bfbcacd2c466b5cfb254
Peter Frentrup <peter_frentrup@gmx.de>
2020-04-02 15:38:39 +0200

    fix: missing #include directive for _pmath_dispatch_tables_memory_panic

--- library/src/pmath-util/memory.c
+++ library/src/pmath-util/memory.c
@@ -14,6 +14,7 @@
 #include <pmath-util/concurrency/threadlocks-private.h>
 #include <pmath-util/concurrency/threads.h>
 #include <pmath-util/debug.h>
+#include <pmath-util/dispatch-table-private.h>
 #include <pmath-util/overflow-calc-private.h>
 
 #include <stdio.h>
