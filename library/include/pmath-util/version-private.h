4e491867c40914f1729a1e050c38d508e98b1036
Peter Frentrup <peter_frentrup@gmx.de>
2020-11-01 16:59:40 +0100

    implement TabView

--- library/include/pmath-util/version-private.h
+++ library/include/pmath-util/version-private.h
@@ -2,10 +2,10 @@
 #define __PMATH_UTIL__VERSION_PRIVATE_H__
 
 	//Date Version Types
-	static const char _PMATH_VERSION_DATE[] = "25";
-	static const char _PMATH_VERSION_MONTH[] = "10";
+	static const char _PMATH_VERSION_DATE[] = "01";
+	static const char _PMATH_VERSION_MONTH[] = "11";
 	static const char _PMATH_VERSION_YEAR[] = "2020";
-	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] =  "20.10";
+	static const char _PMATH_VERSION_UBUNTU_VERSION_STYLE[] =  "20.11";
 	
 	//Software Status
 	static const char _PMATH_VERSION_STATUS[] =  "Alpha";
@@ -14,14 +14,14 @@
 	//Standard Version Type
 	static const long _PMATH_VERSION_MAJOR  = 0;
 	static const long _PMATH_VERSION_MINOR  = 5;
-	static const long _PMATH_VERSION_BUILD  = 879;
-	static const long _PMATH_VERSION_REVISION  = 1287;
+	static const long _PMATH_VERSION_BUILD  = 882;
+	static const long _PMATH_VERSION_REVISION  = 1290;
 	
 	//Miscellaneous Version Types
-	static const long _PMATH_VERSION_BUILDS_COUNT  = 1083;
-	#define _PMATH_VERSION_RC_FILEVERSION 0,5,879,1287
-	#define _PMATH_VERSION_RC_FILEVERSION_STRING "0, 5, 879, 1287\0"
-	static const char _PMATH_VERSION_FULLVERSION_STRING [] = "0.5.879.1287";
+	static const long _PMATH_VERSION_BUILDS_COUNT  = 1085;
+	#define _PMATH_VERSION_RC_FILEVERSION 0,5,882,1290
+	#define _PMATH_VERSION_RC_FILEVERSION_STRING "0, 5, 882, 1290\0"
+	static const char _PMATH_VERSION_FULLVERSION_STRING [] = "0.5.882.1290";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long _PMATH_VERSION_BUILD_HISTORY  = 0;
