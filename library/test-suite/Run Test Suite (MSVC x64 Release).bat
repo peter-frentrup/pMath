9b9b5a41f77ec490bc0d16d4297b7a9482cbd618
Peter Frentrup <peter_frentrup@gmx.de>
2021-04-04 11:36:31 +0200

    test suite: only quit if no message was generated

--- library/test-suite/Run Test Suite (MSVC x64 Release).bat
+++ library/test-suite/Run Test Suite (MSVC x64 Release).bat
@@ -1,6 +1,7 @@
 @echo off
 
 rem %~dp0..\test\bin\windows\msvc-release-x64\test.exe -q -l run-all.pmath
-%~dp0..\console-win\bin\windows\msvc-release-x64\console-win.exe --quit --load run-all.pmath
+rem %~dp0..\console-win\bin\windows\msvc-release-x64\console-win.exe --quit --load run-all.pmath
+%~dp0..\console-win\bin\windows\msvc-release-x64\console-win.exe --load run-all.pmath
 
 pause
