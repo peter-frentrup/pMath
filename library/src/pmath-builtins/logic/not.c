02230cdac22d87a3c20532cac6be609501468690
Peter Frentrup <peter_frentrup@gmx.de>
2024-04-01 14:25:48 +0200

    basic simplifications for Not

--- library/src/pmath-builtins/logic/not.c
+++ library/src/pmath-builtins/logic/not.c
@@ -3,10 +3,15 @@
 #include <pmath-builtins/all-symbols-private.h>
 
 
+extern pmath_symbol_t pmath_System_Equal;
 extern pmath_symbol_t pmath_System_False;
 extern pmath_symbol_t pmath_System_Not;
 extern pmath_symbol_t pmath_System_True;
 extern pmath_symbol_t pmath_System_Undefined;
+extern pmath_symbol_t pmath_System_Unequal;
+
+
+static pmath_symbol_t opposite_inequality(pmath_symbol_t head);
 
 PMATH_PRIVATE pmath_t builtin_not(pmath_expr_t expr) {
   pmath_t item;
@@ -36,20 +41,37 @@ PMATH_PRIVATE pmath_t builtin_not(pmath_expr_t expr) {
   
   if(pmath_is_expr(item)) {
     pmath_t head = pmath_expr_get_item(item, 0);
+    pmath_unref(head);
     
-    if( pmath_same(head, pmath_System_Not) &&
-        pmath_expr_length(item) == 1)
-    {
+    size_t len = pmath_expr_length(item);
+    
+    if(len == 1 && pmath_same(head, pmath_System_Not)) {
       pmath_t result = pmath_expr_get_item(item, 1);
-      pmath_unref(head);
       pmath_unref(item);
       pmath_unref(expr);
       return result;
     }
-    
-    pmath_unref(head);
+    else if(len == 2) {
+      head = opposite_inequality(head);
+      if(!pmath_same(head, PMATH_UNDEFINED)) {
+        pmath_unref(expr);
+        item = pmath_expr_set_item(item, 0, pmath_ref(head));
+        return item;
+      }
+    }
   }
   pmath_unref(item);
   
   return expr;
+}
+
+
+static pmath_symbol_t opposite_inequality(pmath_symbol_t head) { // won't be freed; must call pmath_ref() on result.
+  if(pmath_same(head, pmath_System_Equal))   return pmath_System_Unequal;
+  if(pmath_same(head, pmath_System_Unequal)) return pmath_System_Equal;
+  
+  // TODO: real inqaulities only if they do not apply at all to complex numbers. 
+  // E.g. we could define "2 < ImaginaryI" to be False, in which case "Not(x < y)" may *not* simplify to "x >= y".
+  // In that case we might want to simplify using heads NotLess, NotLessEqual, NotGreater etc.
+  return PMATH_UNDEFINED;
 }
