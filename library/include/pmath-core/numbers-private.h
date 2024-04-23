c0097b0e1b49d8ad027627e509a50e8cad692224
Peter Frentrup <peter_frentrup@gmx.de>
2024-04-23 22:17:01 +0200

    do not warn if M_PI or M_E are not defined

--- library/include/pmath-core/numbers-private.h
+++ library/include/pmath-core/numbers-private.h
@@ -66,12 +66,10 @@
 #endif
 
 #ifndef M_PI
-#  warning "M_PI not defined"
 #  define M_PI    3.14159265358979323846
 #endif
 
 #ifndef M_E
-#  warning "M_E not defined"
 #  define M_E   2.7182818284590452354
 #endif
 
