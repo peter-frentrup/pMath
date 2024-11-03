611b59a18e1882f4a8ad6cca3cec673fdb499ba4
Peter Frentrup <peter_frentrup@gmx.de>
2024-11-03 21:55:24 +0100

    make FilterRules aware of association lists. Not perfect yet

--- library/src/pmath-builtins/control/filterrules.c
+++ library/src/pmath-builtins/control/filterrules.c
@@ -1,5 +1,6 @@
 #include <pmath-language/patterns-private.h>
 
+#include <pmath-util/association-lists.h>
 #include <pmath-util/dispatch-tables.h>
 #include <pmath-util/helpers.h>
 #include <pmath-util/messages.h>
@@ -11,7 +12,13 @@ extern pmath_symbol_t pmath_System_Alternatives;
 extern pmath_symbol_t pmath_System_Except;
 extern pmath_symbol_t pmath_System_List;
 
-static pmath_t prepare_pattern(pmath_t pattern){ // will be freed
+static pmath_t prepare_pattern(pmath_t pattern) { // will be freed
+  if(pmath_is_association_list(pattern)) {
+    pmath_t keys = pmath_association_list_get_keys(pattern);
+    pmath_unref(pattern);
+    return pmath_expr_set_item(keys, 0, pmath_ref(pmath_System_Alternatives));
+  }
+
   if(pmath_is_expr_of(pattern, pmath_System_List)){
     size_t i;
     pattern = pmath_expr_set_item(pattern, 0, pmath_ref(pmath_System_Alternatives));
@@ -78,12 +85,14 @@ PMATH_PRIVATE pmath_t builtin_filterrules(pmath_expr_t expr){
   
   pattern = prepare_pattern(pattern);
   
+  pmath_bool_t any_change = FALSE;
   for(i = pmath_expr_length(rules);i > 0;--i){
     pmath_t rule = pmath_expr_get_item(rules, i);
     pmath_t lhs  = pmath_expr_get_item(rule, 1);
     pmath_unref(rule);
     
-    if(!_pmath_pattern_match(lhs, pmath_ref(pattern), NULL)){
+    if(!_pmath_pattern_match(lhs, pmath_ref(pattern), NULL)) {
+      any_change = TRUE;
       rules = pmath_expr_set_item(rules, i, PMATH_UNDEFINED);
     }
     
@@ -91,5 +100,10 @@ PMATH_PRIVATE pmath_t builtin_filterrules(pmath_expr_t expr){
   }
   
   pmath_unref(pattern);
-  return pmath_expr_remove_all(rules, PMATH_UNDEFINED);
+
+  if(any_change) {
+    rules = pmath_expr_remove_all(rules, PMATH_UNDEFINED);
+  }
+  pmath_try_make_association_list(&rules);
+  return rules;
 }
