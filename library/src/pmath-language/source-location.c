13c68e4f3d9b6bbfc86d491c513a6bda72c75205
Peter Frentrup <peter_frentrup@gmx.de>
2024-11-10 23:26:56 +0100

    massively reduce memory requirements for Language`SourceLocation metadata by using a custom expr representation (see issue #154)
    fresh x64 richmath.exe uses 37% less memory and 44% less peak memory (old MemoryUsage() after start: {6718887, 10749386}, new: {4196159, 5999770})

--- library/src/pmath-language/source-location.c
+++ library/src/pmath-language/source-location.c
@@ -1,7 +1,7 @@
 #include <pmath-language/source-location.h>
 
 #include <pmath-core/objects-inline.h>
-#include <pmath-core/expressions.h>
+#include <pmath-core/expressions-private.h>
 #include <pmath-core/symbols.h>
 
 #include <pmath-util/helpers.h>
@@ -11,25 +11,102 @@ extern pmath_symbol_t pmath_System_Range;
 extern pmath_symbol_t pmath_Language_SourceLocation;
 
 
+struct _pmath_source_location_extra_data_t {
+  struct _pmath_custom_expr_data_t base;
+  
+  int start;
+  int end;
+  int startcol;
+  int endcol;
+};
+
+#define SRCLOC_EXPR_EXTRA(EXPR_PTR)      ((struct _pmath_source_location_extra_data_t*)PMATH_CUSTOM_EXPR_DATA(EXPR_PTR))
+
+
+static void    source_location_destroy_data(struct _pmath_custom_expr_data_t *_data);
+static size_t  source_location_get_length(  struct _pmath_custom_expr_t *e);
+static pmath_t source_location_get_item(    struct _pmath_custom_expr_t *e, size_t i);
+
+static const struct _pmath_custom_expr_api_t source_location_expr_api = {
+  .destroy_data = source_location_destroy_data,
+  .get_length   = source_location_get_length,
+  .get_item     = source_location_get_item,
+};
+
 PMATH_API
 pmath_t pmath_language_new_file_location(pmath_t ref, int startline, int startcol, int endline, int endcol) {
-  return pmath_expr_new_extended(
-           pmath_ref(pmath_Language_SourceLocation), 2,
-           ref,
-           pmath_expr_new_extended(
-             pmath_ref(pmath_System_Range), 2,
-             pmath_build_value("(ii)", startline, startcol),
-             pmath_build_value("(ii)", endline,   endcol)));
+  struct _pmath_custom_expr_t *result;
+  
+  result = _pmath_expr_new_custom(0, &source_location_expr_api, sizeof(struct _pmath_source_location_extra_data_t));
+  if(!result) {
+    pmath_unref(ref);
+    return PMATH_NULL;
+  }
+  
+  result->internals.items[0] = ref;
+  
+  struct _pmath_source_location_extra_data_t *data = SRCLOC_EXPR_EXTRA(result);
+  data->start    = startline;
+  data->end      = endline;
+  data->startcol = startcol;
+  data->endcol   = endcol;
+
+  return PMATH_FROM_PTR(result);
 }
 
 PMATH_API
 pmath_t pmath_language_new_simple_location(pmath_t ref, int start, int end) {
-  return pmath_expr_new_extended(
-           pmath_ref(pmath_Language_SourceLocation), 2,
-           ref,
-           pmath_expr_new_extended(
-             pmath_ref(pmath_System_Range), 2,
-             PMATH_FROM_INT32(start),
-             PMATH_FROM_INT32(end)));
+  struct _pmath_custom_expr_t *result;
+  
+  result = _pmath_expr_new_custom(0, &source_location_expr_api, sizeof(struct _pmath_source_location_extra_data_t));
+  if(!result) {
+    pmath_unref(ref);
+    return PMATH_NULL;
+  }
+  
+  result->internals.items[0] = ref;
+  
+  struct _pmath_source_location_extra_data_t *data = SRCLOC_EXPR_EXTRA(result);
+  data->start    = start;
+  data->end      = end;
+  data->startcol = -1;
+  data->endcol   = -1;
+
+  return PMATH_FROM_PTR(result);
 }
 
+static void source_location_destroy_data(struct _pmath_custom_expr_data_t *_data) {
+  //struct _pmath_source_location_extra_data_t *data = (void*)_data;
+}
+
+static size_t source_location_get_length(struct _pmath_custom_expr_t *e) {
+  //struct _pmath_source_location_extra_data_t *data = SRCLOC_EXPR_EXTRA(e);
+  
+  return 2;
+}
+
+static pmath_t source_location_get_item(struct _pmath_custom_expr_t *e, size_t i) {
+  struct _pmath_source_location_extra_data_t *data = SRCLOC_EXPR_EXTRA(e);
+  
+  if(i == 0)
+    return pmath_ref(pmath_Language_SourceLocation);
+  
+  if(i == 1)
+    return pmath_ref(e->internals.items[0]);
+  
+  if(i > 2)
+    return PMATH_NULL;
+  
+  if(data->startcol == -1 && data->endcol == -1) {
+    return pmath_expr_new_extended(
+             pmath_ref(pmath_System_Range), 2, 
+             PMATH_FROM_INT32(data->start),
+             PMATH_FROM_INT32(data->end));
+  }
+  else {
+    return pmath_expr_new_extended(
+             pmath_ref(pmath_System_Range), 2,
+             pmath_build_value("(ii)", data->start, data->startcol),
+             pmath_build_value("(ii)", data->end,   data->endcol));
+  }
+}
