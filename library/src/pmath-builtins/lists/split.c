c0136ff2994cf81b37a0d4a6006affd4dc67a42c
Peter Frentrup <peter_frentrup@gmx.de>
2026-01-25 11:55:19 +0100

    refactor Split and let it compare consecutive elements so that it can be used to e.g. split into monotonic sequences

--- library/src/pmath-builtins/lists/split.c
+++ library/src/pmath-builtins/lists/split.c
@@ -9,11 +9,28 @@
 
 extern pmath_symbol_t pmath_System_True;
 
+// list will be freed:
+static pmath_t split_by(pmath_expr_t list, pmath_bool_t (*same_test)(pmath_t, pmath_t, void*), void *ctx);
+
+static pmath_bool_t is_identical(pmath_t a, pmath_t b, void *_ignored) {
+  return pmath_equals(a, b);
+}
+static pmath_bool_t call_same_test(pmath_t a, pmath_t b, void *_test_ptr) {
+  pmath_t *test_ptr = _test_ptr;
+  
+  pmath_t test_result = pmath_evaluate(pmath_expr_new_extended(
+                          pmath_ref(*test_ptr), 2,
+                          pmath_ref(a),
+                          pmath_ref(b)));
+  pmath_unref(test_result);
+  
+  return pmath_same(test_result, pmath_System_True);
+}
+
 PMATH_PRIVATE pmath_t builtin_split(pmath_expr_t expr) {
   /* Split(list, test)
      Split(list)        == Split(expr, Identical)
    */
-  size_t i;
   size_t exprlen = pmath_expr_length(expr);
   pmath_t list, head, test;
   
@@ -32,101 +49,50 @@ PMATH_PRIVATE pmath_t builtin_split(pmath_expr_t expr) {
   head = pmath_expr_get_item(list, 0);
   
   if(exprlen == 1) {
-    pmath_gather_begin(PMATH_NULL);
-    
     pmath_unref(expr);
-    i = 1;
-    while(i <= pmath_expr_length(list)) {
-      size_t j;
-      pmath_t a = pmath_expr_get_item(list, i);
-      
-      pmath_gather_begin(PMATH_NULL);
-      pmath_emit(pmath_ref(a), PMATH_NULL);
-      
-      for(j = i + 1;; ++j) {
-        pmath_t b;
-        
-        if(j > pmath_expr_length(list)) {
-          b = pmath_gather_end();
-          b = pmath_expr_set_item(b, 0, pmath_ref(head));
-          pmath_emit(b, PMATH_NULL);
-          break;
-        }
-        
-        b = pmath_expr_get_item(list, j);
-        
-        if(!pmath_equals(a, b)) {
-          pmath_unref(b);
-          
-          b = pmath_gather_end();
-          b = pmath_expr_set_item(b, 0, pmath_ref(head));
-          pmath_emit(b, PMATH_NULL);
-          break;
-        }
-        else
-          pmath_emit(b, PMATH_NULL);
-      }
-      
-      pmath_unref(a);
-      i = j;
-    }
+    list = split_by(list, is_identical, NULL);
+  }
+  else {
+    test = pmath_expr_get_item(expr, 2);
+    pmath_unref(expr);
     
-    pmath_unref(list);
-    list = pmath_gather_end();
-    list = pmath_expr_set_item(list, 0, head);
-    return list;
+    list = split_by(list, call_same_test, &test);
+    pmath_unref(test);
   }
   
-  test = pmath_expr_get_item(expr, 2);
-  pmath_unref(expr);
-  pmath_gather_begin(PMATH_NULL);
+  list = pmath_expr_set_item(list, 0, head);
+  return list;
+}
+
+static pmath_t split_by(pmath_expr_t list, pmath_bool_t (*same_test)(pmath_t, pmath_t, void*), void *ctx) {
+  size_t len = pmath_expr_length(list);
   
-  i = 1;
-  while(i <= pmath_expr_length(list)) {
-    size_t j;
-    pmath_t a = pmath_expr_get_item(list, i);
-    
-    pmath_gather_begin(PMATH_NULL);
-    pmath_emit(pmath_ref(a), PMATH_NULL);
-    
-    for(j = i + 1;; ++j) {
-      pmath_t b;
-      pmath_t test_result;
-      
-      if(j > pmath_expr_length(list)) {
-        b = pmath_gather_end();
-        b = pmath_expr_set_item(b, 0, pmath_ref(head));
-        pmath_emit(b, PMATH_NULL);
-        break;
-      }
-      
-      b = pmath_expr_get_item(list, j);
-      
-      test_result = pmath_evaluate(pmath_expr_new_extended(
-                                     pmath_ref(test), 2,
-                                     pmath_ref(a),
-                                     pmath_ref(b)));
-      pmath_unref(test_result);
-      
-      if(!pmath_same(test_result, pmath_System_True)) {
-        pmath_unref(b);
+  pmath_gather_begin(PMATH_NULL);
+  if(len >= 1) {
+    size_t start = 1;
+    pmath_t a = pmath_expr_get_item(list, start);
+    while(start <= len) {
+      size_t next;
+      for(next = start + 1; next <= len; ++next) {
+        pmath_t b = pmath_expr_get_item(list, next);
         
-        b = pmath_gather_end();
-        b = pmath_expr_set_item(b, 0, pmath_ref(head));
-        pmath_emit(b, PMATH_NULL);
-        break;
+        if(same_test(a, b, ctx)) {
+          pmath_unref(a);
+          a = b;
+        }
+        else {
+          pmath_unref(a);
+          a = b;
+          break;
+        }
       }
-      else
-        pmath_emit(b, PMATH_NULL);
+      
+      pmath_emit(pmath_expr_get_item_range(list, start, next - start), PMATH_NULL);
+      start = next;
     }
-    
     pmath_unref(a);
-    i = j;
   }
   
   pmath_unref(list);
-  pmath_unref(test);
-  list = pmath_gather_end();
-  list = pmath_expr_set_item(list, 0, head);
-  return list;
+  return pmath_gather_end();
 }
